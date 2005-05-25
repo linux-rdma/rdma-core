2b8cdf2cb1a96148d529af95b393312c42fccfbe
Hal Rosenstock <halr@voltaire.com>
2005-05-25 12:41:39 +0000

    r2478: Added preference component mask bit for path record
    Signed-off-by: Hal Rosenstock <halr@voltaire.com>

--- src/sa.c
+++ src/sa.c
@@ -104,6 +104,7 @@ sa_call(void *rcvbuf, ib_portid_t *portid, ib_sa_call_t *sa, uint timeout)
 #define	IB_PR_COMPMASK_RATE				(1ull<<19)
 #define	IB_PR_COMPMASK_PKTLIFETIMESELEC			(1ull<<20)
 #define	IB_PR_COMPMASK_PKTLIFETIME			(1ull<<21)
+#define	IB_PR_COMPMASK_PREFERENCE			(1ull<<22)
 
 #define IB_PR_DEF_MASK (IB_PR_COMPMASK_DGID |\
 			IB_PR_COMPMASK_SGID |\
