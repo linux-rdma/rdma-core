f83b40c86831794ec7bb9ac234ba41fe8c40c4e6
Hal Rosenstock <halr@voltaire.com>
2007-03-23 09:14:36 -0500

    libibmad/mad.c: Implement GRH support in mad_build_pkt
    Signed-off-by: Hal Rosenstock <halr@voltaire.com>

--- src/mad.c
+++ src/mad.c
@@ -175,6 +175,7 @@ mad_build_pkt(void *umad, ib_rpc_t *rpc, ib_portid_t *dport,
 	int lid_routed = rpc->mgtclass != IB_SMI_DIRECT_CLASS;
 	int is_smi = (rpc->mgtclass == IB_SMI_CLASS ||
 		      rpc->mgtclass == IB_SMI_DIRECT_CLASS);
+	struct ib_mad_addr addr;
 
 	if (!is_smi)
 		umad_set_addr(umad, dport->lid, dport->qp, dport->sl, dport->qkey);
@@ -186,7 +187,15 @@ mad_build_pkt(void *umad, ib_rpc_t *rpc, ib_portid_t *dport,
 		else
 			umad_set_addr(umad, 0xffff, 0, 0, 0);
 
-	umad_set_grh(umad, (dport->grh_present && !is_smi) ? 0/*grh*/ : 0);	/* FIXME: GRH support */
+	if (dport->grh_present && !is_smi) {
+		addr.grh_present = 1;
+		memcpy(addr.gid, dport->gid, 16);
+		addr.hop_limit = 0xff;
+		addr.traffic_class = 0;
+		addr.flow_label = 0;
+		umad_set_grh(umad, &addr);
+	} else
+		umad_set_grh(umad, 0);
 	umad_set_pkey(umad, is_smi ? 0 : dport->pkey_idx);
 
 	mad = umad_get_mad(umad);
