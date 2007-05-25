c156628cb66af6273299cf25d2df3b467c3034c8
Hal Rosenstock <halr@voltaire.com>
2007-05-25 19:31:35 -0400

    infiniband-diags/scripts/IBswcountlimits.pm: Add support for routers
    Signed-off-by: Hal Rosenstock <halr@voltaire.com>

--- scripts/IBswcountlimits.pm
+++ scripts/IBswcountlimits.pm
@@ -249,7 +249,7 @@ sub get_link_ends
       if ( $in_switch eq "yes" )
       {
 	 my $rec = undef;
-         if ($line =~ /^\[(\d+)\]\s+\"[HS]-(.+)\"\[(\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
+         if ($line =~ /^\[(\d+)\]\s+\"[HSR]-(.+)\"\[(\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
          {
             $loc_port = $1;
             my $rem_guid = $2;
@@ -261,7 +261,7 @@ sub get_link_ends
                   rem_guid => "0x$rem_guid", rem_lid => $rem_lid, rem_port => $rem_port, rem_ext_port => "", rem_desc => $rem_desc };
             #print "$rec->{loc_desc}\n";
          }
-         if ($line =~ /^\[(\d+)\]\[ext (\d+)\]\s+\"[HS]-(.+)\"\[(\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
+         if ($line =~ /^\[(\d+)\]\[ext (\d+)\]\s+\"[HSR]-(.+)\"\[(\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
          {
             $loc_port = $1;
             my $loc_ext_port = $2;
@@ -273,7 +273,7 @@ sub get_link_ends
                         loc_sw_lid => $loc_sw_lid,
                   rem_guid => "0x$rem_guid", rem_lid => $rem_lid, rem_port => $rem_port, rem_ext_port => "", rem_desc => $rem_desc };
          }
-         if ($line =~ /^\[(\d+)\]\s+\"[HS]-(.+)\"\[(\d+)\]\[ext (\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
+         if ($line =~ /^\[(\d+)\]\s+\"[HSR]-(.+)\"\[(\d+)\]\[ext (\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
          {
             $loc_port = $1;
             my $rem_guid = $2;
@@ -285,7 +285,7 @@ sub get_link_ends
                         loc_sw_lid => $loc_sw_lid,
                   rem_guid => "0x$rem_guid", rem_lid => $rem_lid, rem_port => $rem_port, rem_ext_port => $rem_ext_port, rem_desc => $rem_desc };
          }
-         if ($line =~ /^\[(\d+)\]\[ext (\d+)\]\s+\"[HS]-(.+)\"\[(\d+)\]\[ext (\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
+         if ($line =~ /^\[(\d+)\]\[ext (\d+)\]\s+\"[HSR]-(.+)\"\[(\d+)\]\[ext (\d+)\]\s+#.*\"(.*)\"\.* lid (\d+).*/)
          {
             $loc_port = $1;
             my $loc_ext_port = $2;
