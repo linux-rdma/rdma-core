eb1a85399667b3a78e233898d91b3d3bfd8bbd5b
Ira Weiny <weiny2@llnl.gov>
2007-08-09 19:03:31 -0700

    infiniband-diags/set_mthca_nodedesc.sh: change to work for all HCA's
    From 3abf9ac10ee90f7272969486704c2989d254a2ac Mon Sep 17 00:00:00 2001
    From: Ira K. Weiny <weiny2@llnl.gov>
    Date: Thu, 9 Aug 2007 16:07:38 -0700
    Subject: [PATCH] Modify set_mthca_nodedesc to set the node_desc for any hca type found in sysfs.
    
    This has been tested for the MLX4 and mthca cards but not others.
    
    Signed-off-by: Ira K. Weiny <weiny2@llnl.gov>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- scripts/set_mthca_nodedesc.sh
+++ scripts/set_mthca_nodedesc.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-# set the node_desc field in the mthca to the hostname
+# set the node_desc field of any hca found to the defined hostname
 
 . /etc/sysconfig/network
 
-echo -n "$HOSTNAME" >> /sys/class/infiniband/mthca0/node_desc
+ib_sysfs="/sys/class/infiniband"
+
+for hca in `ls $ib_sysfs`; do
+   if [ -f $ib_sysfs/$hca/node_desc ]; then
+      echo -n "$HOSTNAME" >> $ib_sysfs/$hca/node_desc
+   else
+      logger -s "Failed to set node_desc for : $hca"
+   fi
+done
 
 exit 0
