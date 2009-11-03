abc34ab016bb8f006d394a24c2107ec08b2fab7c
Al Chu <chu11@llnl.gov>
2009-11-03 10:37:29 -0800

    remove unused headers in libibnetdiscover
    Hey Sasha,
    
    Just noticed it.  Left over/cut and pasted from the original
    ibnetdiscover tool code or no longer necessary due to recent changes.
    This patch is independent of the other patches I recently posted.
    
    Al
    
    --
    Albert Chu
    chu11@llnl.gov
    Computer Scientist
    High Performance Systems Division
    Lawrence Livermore National Laboratory
    
    From: Albert Chu <chu11@llnl.gov>
    Date: Tue, 3 Nov 2009 10:33:08 -0800
    Subject: [PATCH] remove unused headers
    
    Signed-off-by: Albert Chu <chu11@llnl.gov>
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- libibnetdisc/src/ibnetdisc.c
+++ libibnetdisc/src/ibnetdisc.c
@@ -41,10 +41,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdarg.h>
-#include <time.h>
 #include <string.h>
-#include <getopt.h>
 #include <errno.h>
 #include <inttypes.h>
 
