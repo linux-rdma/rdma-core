b1e4531e2d10a8404cc7873c6f899494cef236c4
Alex Netes <alexne@voltaire.com>
2011-02-03 18:25:19 +0200

    Makefile: ChangeLog and version generation script path fix
    Move ChangeLog and version generation script into top source directory and fix
    path in Makefile accordingly.
    
    Signed-off-by: Alex Netes <alexne@voltaire.com>

---
+++ gen_ver.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# This generates a version string which includes recent version as
+# specified in correspondent sub project's configure.in file, plus
+# git revision abbreviation in the case if sub-project HEAD is different
+# from recent tag, plus "-dirty" suffix if local uncommitted changes are
+# in the sub project tree.
+#
+
+usage()
+{
+	echo "Usage: $0"
+	exit 2
+}
+
+cd `dirname $0`
+
+packege=`basename \`pwd\``
+conf_file=configure.in
+version=`cat $conf_file | sed -ne '/AC_INIT.*.*/s/^AC_INIT.*, \(.*\),.*$/\1/p'`
+
+git diff --quiet $packege-$version..HEAD -- ./ > /dev/null 2>&1
+if [ $? -eq 1 ] ; then
+	abbr=`git rev-parse --short --verify HEAD 2>/dev/null`
+	if [ ! -z "$abbr" ] ; then
+		version="${version}_${abbr}"
+	fi
+fi
+
+git diff-index --quiet HEAD -- ./> /dev/null 2>&1
+if [ $? -eq 1 ] ; then
+	version="${version}_dirty"
+fi
+
+echo $version
