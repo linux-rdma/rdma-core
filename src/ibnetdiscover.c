3133743b5ea6a7d81b7d70e8a83ce4eb28fec8e6
Hal Rosenstock <hal@dev.mellanox.co.il>
2011-08-29 21:18:31 +0000

    infiniband-diags/ibnetdiscover.c: Fix cap_mask endian for extended link speeds
    Signed-off-by: Daniel Klein <danielk@mellanox.co.il>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Ira Weiny <weiny2@llnl.gov>

--- src/ibnetdiscover.c
+++ src/ibnetdiscover.c
@@ -375,7 +375,7 @@ void out_switch_port(ibnd_port_t * port, int group, char *out_prefix)
 	ext_port_str = out_ext_port(port->remoteport, group);
 	cap_mask = mad_get_field(port->node->ports[0]->info, 0,
 				 IB_PORT_CAPMASK_F);
-	if (cap_mask & IB_PORT_CAP_HAS_EXT_SPEEDS)
+	if (cap_mask & CL_NTOH32(IB_PORT_CAP_HAS_EXT_SPEEDS))
 		espeed = mad_get_field(port->info, 0,
 				       IB_PORT_LINK_SPEED_EXT_ACTIVE_F);
 	else
@@ -433,7 +433,7 @@ void out_ca_port(ibnd_port_t * port, int group, char *out_prefix)
 				       port->remoteport->node->nodedesc);
 
 	cap_mask = mad_get_field(port->info, 0, IB_PORT_CAPMASK_F);
-	if (cap_mask & IB_PORT_CAP_HAS_EXT_SPEEDS)
+	if (cap_mask & CL_NTOH32(IB_PORT_CAP_HAS_EXT_SPEEDS))
 		espeed = mad_get_field(port->info, 0,
 				       IB_PORT_LINK_SPEED_EXT_ACTIVE_F);
 	else
@@ -684,7 +684,7 @@ void dump_ports_report(ibnd_node_t * node, void *user_data)
 		else
 			info = (uint8_t *)&port->info;
 		cap_mask = mad_get_field(info, 0, IB_PORT_CAPMASK_F);
-		if (cap_mask & IB_PORT_CAP_HAS_EXT_SPEEDS)
+		if (cap_mask & CL_NTOH32(IB_PORT_CAP_HAS_EXT_SPEEDS))
 			espeed = mad_get_field(port->info, 0,
 					       IB_PORT_LINK_SPEED_EXT_ACTIVE_F);
 		else
