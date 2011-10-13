4ed1bbb8e642d88cca5dbbd2b5c6fb5473f45265
Albert L.Chu <chu11@llnl.gov>
2011-10-12 17:17:08 -0700

    Add -e heuristic flag to check_lft_balance.pl to detect common scenarios where unbalanced routing will occur.
    Signed-off-by: Albert L. Chu <chu11@llnl.gov>
    Signed-off-by: Ira Weiny <weiny2@llnl.gov>

--- scripts/check_lft_balance.pl
+++ scripts/check_lft_balance.pl
@@ -60,14 +60,17 @@ my $lft_line;
 my $lids_per_port;
 my $lids_per_port_calculated;
 
+my $heuristic_flag = 0;
+
 sub usage
 {
 	my $prog = `basename $0`;
 
 	chomp($prog);
-	print "Usage: $prog -l lft-output -i ibnetdiscover-cache [-v]\n";
+	print "Usage: $prog -l lft-output -i ibnetdiscover-cache [-e] [-v]\n";
 	print "  Generate lft-output via \"dump_lfts.sh > lft-output\"\n";
 	print "  Generate ibnetdiscover-cache via \"ibnetdiscover --cache ibnetdiscover-cache\"\n";
+	print "  -e turn on heuristic(s) to look at switch balances deeper\n";
 	print "  -v verbose output, output all switches\n";
 	exit 2;
 }
@@ -154,6 +157,8 @@ sub output_switch_port_usage
 {
 	my $min_usage = 999999;
 	my $max_usage = 0;
+	my $min_usage2 = 999999;
+	my $max_usage2 = 0;
 	my @ports     = (
 		"001", "002", "003", "004", "005", "006", "007", "008",
 		"009", "010", "011", "012", "013", "014", "015", "016",
@@ -162,8 +167,11 @@ sub output_switch_port_usage
 		"033", "034", "035", "036"
 	);
 	my @output_ports = ();
+	my @double_check_ports = ();
 	my $port;
 	my $iblinkinfo_output;
+	my $is_unbalanced = 0;
+	my $ports_on_switch = 0;
 	my $ret;
 
         $iblinkinfo_output = `iblinkinfo --load-cache $ibnetdiscover_cache -S $switch_guid`;
@@ -180,6 +188,8 @@ sub output_switch_port_usage
 				next;
 			}
 		}
+
+		$ports_on_switch++;
 
 		# If port is directly connected to a node, don't use
 		# it in this calculation.
@@ -198,8 +208,53 @@ sub output_switch_port_usage
 		}
 	}
 
-	if ($verbose || ($max_usage > ($min_usage + 1))) {
-		if ($max_usage > ($min_usage + 1)) {
+	if ($max_usage > ($min_usage + 1)) {
+		$is_unbalanced = 1;
+	}
+
+	# In the event this is a switch lineboard, it will almost always never
+	# balanced.  Half the ports go up to the spine, and the rest of the ports
+	# go down to HCAs.  So we will do a special heuristic:
+	#
+	# If about 1/2 of the remaining ports are balanced, then we will consider the
+	# entire switch balanced.
+	#
+	# Also, we do this only if there are enough alive ports on the switch to care.
+	# I picked 12 somewhat randomly
+	if ($heuristic_flag == 1
+	    && $is_unbalanced == 1
+	    && $ports_on_switch > 12) {
+		for $port (@output_ports) {
+			if ($switch_port_count{$port} == $max_usage
+			    || $switch_port_count{$port} == ($max_usage - 1)
+			    || $switch_port_count{$port} == 0) {
+				next;
+			}
+
+			push(@double_check_ports, $port);
+		}
+
+		# we'll call half +/- 1 "about half"
+		if (@double_check_ports == int($ports_on_switch / 2)
+		    || @double_check_ports == int($ports_on_switch / 2) + 1
+		    || @double_check_ports == int($ports_on_switch / 2) - 1) {
+			for $port (@double_check_ports) {
+				if ($switch_port_count{$port} < $min_usage2) {
+					$min_usage2 = $switch_port_count{$port};
+				}
+				if ($switch_port_count{$port} > $max_usage2) {
+					$max_usage2 = $switch_port_count{$port};
+				}
+			}
+
+			if (!($max_usage2 > ($min_usage2 + 1))) {
+				$is_unbalanced = 0;
+			}
+		}
+	}
+
+	if ($verbose || $is_unbalanced == 1) {
+		if ($is_unbalanced == 1) {
 			print "Unbalanced Switch Port Usage: ";
 			print "$switch_name, $switch_guid, $switch_lid\n";
 		} else {
@@ -235,7 +290,7 @@ sub process_host_ports
 	}
 }
 
-if (!getopts("hl:i:v")) {
+if (!getopts("hl:i:ev")) {
 	usage();
 }
 
@@ -261,6 +316,10 @@ if (defined($main::opt_i)) {
 
 if (defined($main::opt_v)) {
 	$verbose = 1;
+}
+
+if (defined($main::opt_e)) {
+	$heuristic_flag = 1;
 }
 
 if (!open(FH, "< $dump_lft_file")) {
