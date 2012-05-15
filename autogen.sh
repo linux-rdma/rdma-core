4d1fab57d2a572dfbd0370b1e0cac5ccfea8ac67
Ira Weiny <weiny2@llnl.gov>
2012-05-15 15:52:26 -0700

    infiniband-diags: fix autogen with new rst documentation
    Signed-off-by: Ira Weiny <weiny2@llnl.gov>

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,15 @@
 # create config dir if not exist
 test -d config || mkdir config
 
+rst2manexe=`which rst2man`
+if [ "$rst2manexe" == "" ]; then
+	echo "ERROR: Building from source requires rst2man to build the man pages"
+fi
+
 set -x
 aclocal -I config
 libtoolize --force --copy
 autoheader
+doc/generate
 automake --foreign --add-missing --copy
 autoconf
