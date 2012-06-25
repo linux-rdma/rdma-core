0680bea236f65373732b82d405787cf979ee5c67
Sean Hefty <sean.hefty@intel.com>
2012-06-25 15:04:52 -0700

    rsocket: Set readfds event if rsocket has been disconnected
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- src/rsocket.c
+++ src/rsocket.c
@@ -1630,7 +1630,7 @@ rs_poll_to_select(int nfds, struct pollfd *fds, fd_set *readfds,
 	int i, cnt = 0;
 
 	for (i = 0; i < nfds; i++) {
-		if (readfds && (fds[i].revents & POLLIN)) {
+		if (readfds && (fds[i].revents & (POLLIN | POLLHUP))) {
 			FD_SET(fds[i].fd, readfds);
 			cnt++;
 		}
