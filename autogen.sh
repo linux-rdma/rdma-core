469d081c2be8eefc3316b6bf2be3d4a8e0356a83
Ira Weiny <weiny2@llnl.gov>
2012-08-15 13:50:05 -0400

    infiniband-diags: add default <man>.in files
    Signed-off-by: Ira Weiny <weiny2@llnl.gov>

--- autogen.sh
+++ autogen.sh
@@ -3,11 +3,6 @@
 # create config dir if not exist
 test -d config || mkdir config
 
-rst2manexe=`which rst2man`
-if [ "$rst2manexe" == "" ]; then
-	echo "ERROR: Building from source requires rst2man to build the man pages"
-fi
-
 set -x
 aclocal -I config
 libtoolize --force --copy
