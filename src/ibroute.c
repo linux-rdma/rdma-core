7bf6e54ffd57faf39ae2d608e0310b103a4a61db
Ira Weiny <weiny2@llnl.gov>
2013-01-22 14:39:18 -0800

    infiniband-diags: ibroute fix resolve failure message
    Signed-off-by: Ira Weiny <weiny2@llnl.gov>

--- src/ibroute.c
+++ src/ibroute.c
@@ -462,7 +462,7 @@ int main(int argc, char **argv)
 
 	if (resolve_portid_str(ibd_ca, ibd_ca_port, &portid, argv[0],
 			       ibd_dest_type, ibd_sm_id, srcport) < 0)
-		IBERROR("can't resolve destination port %s", argv[1]);
+		IBERROR("can't resolve destination port %s", argv[0]);
 
 	if (multicast)
 		err = dump_multicast_tables(&portid, startlid, endlid);
