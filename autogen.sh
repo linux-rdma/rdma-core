91fda21bb8ee3e3ef6ba344d8195c490a7d2f09f
Ira Weiny <ira.weiny@intel.com>
2013-04-03 13:01:01 -0700

    infiniband-diags: updates for new autoconf tools
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -4,9 +4,5 @@
 test -d config || mkdir config
 
 set -x
-aclocal -I config
-libtoolize --force --copy
-autoheader
 doc/generate
-automake --foreign --add-missing --copy
-autoconf
+autoreconf -ifv -I config
