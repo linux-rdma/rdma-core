8c0ec71b83c532a2b7c8d24455798e56b267539f
Hal Rosenstock <hal@dev.mellanox.co.il>
2015-01-14 09:07:10 -0500

    vendstat.c: Add additional vendor IDs to ext_fw_info_device
    Add additional supported device IDs to ext_fw_info_device.
    These IDs are ConnectX-4, ConnectX-IB, and Switch-IB.
    This affects is_ext_fw_info_supported which determine
    which Mellanox specific vendor MADs are issued to a device.
    
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- src/vendstat.c
+++ src/vendstat.c
@@ -145,8 +145,9 @@ typedef struct {
 static uint16_t ext_fw_info_device[][2] = {
 	{0x0245, 0x0245},	/* Switch-X */
 	{0xc738, 0xc738},	/* Switch-X */
+	{0xcb84, 0xcb84},	/* Switch-IB */
 	{0x01b3, 0x01b3},	/* IS-4 */
-	{0x1003, 0x1011},	/* Connect-X */
+	{0x1003, 0x1016},	/* Connect-X */
 	{0x0000, 0x0000}};
 
 static int is_ext_fw_info_supported(uint16_t device_id) {
