4989004236cb59d281c743a4b91275ef555951d8
Hal Rosenstock <hal@dev.mellanox.co.il>
2015-05-07 13:54:52 +0300

    ibdiag_sa.c: In sa_get_handle, handle umad_open_port and umad_register failures
    Treat umad_open_port and umad_register errors as failures for sa_get_handle
    
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- src/ibdiag_sa.c
+++ src/ibdiag_sa.c
@@ -60,18 +60,32 @@ struct sa_handle * sa_get_handle(void)
 		IBWARN("No SM/SA found on port %s:%d",
 			ibd_ca ? "" : ibd_ca,
 			ibd_ca_port);
-		free(handle);
-		return (NULL);
+		goto err;
 	}
 
 	handle->dport.qp = 1;
 	if (!handle->dport.qkey)
 		handle->dport.qkey = IB_DEFAULT_QP1_QKEY;
 
-	handle->fd = umad_open_port(ibd_ca, ibd_ca_port);
-	handle->agent = umad_register(handle->fd, IB_SA_CLASS, 2, 1, NULL);
+	if ((handle->fd = umad_open_port(ibd_ca, ibd_ca_port)) < 0) {
+		IBWARN("umad_open_port on port %s:%d failed",
+			ibd_ca ? "" : ibd_ca,
+			ibd_ca_port);
+		goto err;
+	}
+	if ((handle->agent = umad_register(handle->fd, IB_SA_CLASS, 2, 1, NULL)) < 0) {
+		umad_close_port(handle->fd);
+		IBWARN("umad_register for SA class failed on port %s:%d",
+		       ibd_ca ? "" : ibd_ca,
+		       ibd_ca_port);
+		goto err;
+	}
 
 	return handle;
+
+err:
+	free(handle);
+	return (NULL);
 }
 
 void sa_free_handle(struct sa_handle * h)
