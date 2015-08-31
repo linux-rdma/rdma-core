3f1aed48b0fc27df9615d89399edd0ac623b2017
Dan Ben Yosef <danby@mellanox.com>
2015-08-31 10:49:04 +0300

    perfquery -T (print Extended Speed Counters) times out on nodes supporting RS_FEC capability.
    Before sending ExtendedSpeedCounters PerfMgt GMP, perfquery sends
    PortInfoExtended SMP to get CapMask and FECModeActive fields.
    Upon reception of PortInfoExtended SMP, fec_mode_active field is
    erroneously decoded to 16 bit buffer instead of 32 bit buffer.
    As a result, memory corruption occurs and portid.dlid field is set to 0,
    so that ExtendedSpeedCounters GMP is sent to wrong destination.
    
    Signed-off-by: Dan Ben Yosef <danby@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- src/perfquery.c
+++ src/perfquery.c
@@ -469,7 +469,7 @@ static uint8_t is_rsfec_mode_active(ib_portid_t * portid, int port,
 				  uint16_t cap_mask)
 {
 	uint8_t data[IB_SMP_DATA_SIZE] = { 0 };
-	uint16_t fec_mode_active = 0;
+	uint32_t fec_mode_active = 0;
 	uint32_t pie_capmask = 0;
 	if (cap_mask & IS_PM_RSFEC_COUNTERS_SUP) {
 		if (!is_port_info_extended_supported(portid, port, srcport)) {
@@ -486,7 +486,7 @@ static uint8_t is_rsfec_mode_active(ib_portid_t * portid, int port,
 				 &fec_mode_active);
 		if((pie_capmask &
 		    CL_NTOH32(IB_PORT_EXT_CAP_IS_FEC_MODE_SUPPORTED)) &&
-		   (CL_NTOH16(IB_PORT_EXT_RS_FEC_MODE_ACTIVE) == fec_mode_active))
+		   (CL_NTOH16(IB_PORT_EXT_RS_FEC_MODE_ACTIVE) == (fec_mode_active & 0xffff)))
 			return 1;
 	}
 
