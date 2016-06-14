592f5de7fee5826156307d9823786145cfec68c8
Yishai Hadas <yishaih@mellanox.com>
2016-06-14 12:58:24 +0300

    Add ConnectX-5 to the supported list
    Add ConnectX-5 and its Virtual Function to
    the supported list.
    
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>

--- src/mlx5.c
+++ src/mlx5.c
@@ -79,6 +79,8 @@ struct {
 	HCA(MELLANOX, 4116),	/* ConnectX-4 Virtual Function */
 	HCA(MELLANOX, 4117),	/* ConnectX-4LX */
 	HCA(MELLANOX, 4118),	/* ConnectX-4LX Virtual Function */
+	HCA(MELLANOX, 4119),	/* ConnectX-5 */
+	HCA(MELLANOX, 4120),	/* ConnectX-5 Virtual Function */
 };
 
 uint32_t mlx5_debug_mask = 0;
