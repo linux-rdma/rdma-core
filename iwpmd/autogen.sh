441ed7199e45724fc9d4c22c20afb48cc0b4173f
Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
2016-09-02 15:50:56 -0600

    iwpm: Update autotools enough to build
    auto* drift has rendered it unbuildable on modern distros.
    
    Signed-off-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>

--- iwpmd/autogen.sh
+++ iwpmd/autogen.sh
@@ -1,11 +1,3 @@
 #! /bin/sh
 
-# create config dir if not exist
-test -d config || mkdir config
-
-set -x
-aclocal -I config
-libtoolize --force --copy
-autoheader
-automake --foreign --add-missing --copy
-autoconf
+autoreconf -ifv -I config
