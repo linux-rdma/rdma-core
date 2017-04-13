338d52c79ca6a27ba4772fd7454f16788e4e51d8
Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
2017-04-13 14:44:37 -0600

    sparse: Use __builtin_bswap instead of the glibc version
    Sparse warns if you nest swap expressions as the glibc version
    uses an open coded inline scope:
    
    providers/mthca/cq.c:279:25: warning: symbol '__v' shadows an earlier one
    
    The built in version does not cause this warning.
    
    Signed-off-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>

--- buildlib/sparse-include/endian.h
+++ buildlib/sparse-include/endian.h
@@ -25,19 +25,19 @@
 /* These do not actually work, but this trivially ensures that sparse sees all
  * the types. */
 
-#define htobe16(x) ((__force __be16)__bswap_16 (x))
-#define htole16(x) ((__force __le16)__bswap_16 (x))
-#define be16toh(x) ((uint16_t)__bswap_16 ((__force uint16_t)(__be16)(x)))
-#define le16toh(x) ((uint16_t)__bswap_16 ((__force uint16_t)(__le16)(x)))
+#define htobe16(x) ((__force __be16)__builtin_bswap16(x))
+#define htole16(x) ((__force __le16)__builtin_bswap16(x))
+#define be16toh(x) ((uint16_t)__builtin_bswap16((__force uint16_t)(__be16)(x)))
+#define le16toh(x) ((uint16_t)__builtin_bswap16((__force uint16_t)(__le16)(x)))
 
-#define htobe32(x) ((__force __be32)__bswap_32 (x))
-#define htole32(x) ((__force __le32)__bswap_32 (x))
-#define be32toh(x) ((uint32_t)__bswap_32 ((__force uint32_t)(__be32)(x)))
-#define le32toh(x) ((uint32_t)__bswap_32 ((__force uint32_t)(__le32)(x)))
+#define htobe32(x) ((__force __be32)__builtin_bswap32(x))
+#define htole32(x) ((__force __le32)__builtin_bswap32(x))
+#define be32toh(x) ((uint32_t)__builtin_bswap32((__force uint32_t)(__be32)(x)))
+#define le32toh(x) ((uint32_t)__builtin_bswap32((__force uint32_t)(__le32)(x)))
 
-#define htobe64(x) ((__force __be64)__bswap_64 (x))
-#define htole64(x) ((__force __le64)__bswap_64 (x))
-#define be64toh(x) ((uint64_t)__bswap_64 ((__force uint64_t)(__be64)(x)))
-#define le64toh(x) ((uint64_t)__bswap_64 ((__force uint64_t)(__le64)(x)))
+#define htobe64(x) ((__force __be64)__builtin_bswap64(x))
+#define htole64(x) ((__force __le64)__builtin_bswap64(x))
+#define be64toh(x) ((uint64_t)__builtin_bswap64((__force uint64_t)(__be64)(x)))
+#define le64toh(x) ((uint64_t)__builtin_bswap64((__force uint64_t)(__le64)(x)))
 
 #endif
