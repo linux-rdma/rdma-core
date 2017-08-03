2836afb5dac44e0ad98e7c8d021232c1c33dbe3f
Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
2017-08-03 15:14:13 -0600

    Shim sd_notify when libsystemd is not present
    Signed-off-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>

--- buildlib/fixup-include/systemd-sd-daemon.h
+++ buildlib/fixup-include/systemd-sd-daemon.h
@@ -9,3 +9,8 @@ static inline int sd_is_socket(int fd, int family, int type, int listening)
 {
 	return 0;
 }
+
+static inline int sd_notify(int unset_environment, const char *state)
+{
+	return 0;
+}
