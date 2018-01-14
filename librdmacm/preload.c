56bf901601b4a2e4b10b11f411a04b6ced43daee
Talat Batheesh <talatb@mellanox.com>
2018-01-14 13:43:00 +0200

    librdmacm: Fix off-by-one errors in sscanf
    The format string was off-by-one for prog, dom,
    type and proto in sscanf.
    
    This patch fix it by reducing the size and the format
    string adjusted as detected by using the cppcheck tool.
    
    [rdma-core/librdmacm/preload.c:168]:
    (error) Width 64 given in format string (no. 1)
    is larger than destination buffer 'prog[64]',
    use %63s to prevent overflowing it.
    
    [rdma-core/librdmacm/preload.c:168]:
    (error) Width 16 given in format string (no. 2)
    is larger than destination buffer 'dom[16]',
    use %15s to prevent overflowing it.
    [
    rdma-core/librdmacm/preload.c:168]:
    (error) Width 16 given in format string (no. 3)
    is larger than destination buffer 'type[16]',
    use %15s to prevent overflowing it.
    
    [rdma-core/librdmacm/preload.c:168]:
    (error) Width 16 given in format string (no. 4)
    is larger than destination buffer 'proto[16]',
    use %15s to prevent overflowing it.
    
    Signed-off-by: Talat Batheesh <talatb@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- librdmacm/preload.c
+++ librdmacm/preload.c
@@ -165,7 +165,7 @@ static void scan_config(void)
 		if (line[0] == '#')
 			continue;
 
-		if (sscanf(line, "%64s%16s%16s%16s", prog, dom, type, proto) != 4)
+		if (sscanf(line, "%63s%15s%15s%15s", prog, dom, type, proto) != 4)
 			continue;
 
 		new_config = realloc(config, (config_cnt + 1) *
