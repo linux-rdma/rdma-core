1fe8225905ccd42e9a3ea7fe28ad540030fc30f5
Jack Morgenstein <jackm@dev.mellanox.co.il>
2018-02-08 11:41:43 +0200

    mlx5: Fix referencing uninitialized var in create_qp error flow
    There are situations in the create_qp error flow where the context
    pointer inside the qp object remains uninitialized.
    
    This uninitialized pointer gets dereferenced during the create_qp
    error flow in two places (resulting in a segmentation fault).
    
    The first such dereference is inside the mlx5_free_qp_buf() flow.
    
    Since mlx5_free_qp_buf() is used only in places where the context
    pointer is independently available, fix this by adding ctx as
    a separate parameter to mlx5_free_qp_buf()
    
    The second such dereference is inside function mlx5_alloc_qp_buf().
    If the allocation fails, it passes the (uninitialized) context
    pointer inside the qp object to another function which dereferences it.
    
    In this case, too, the context pointer is independently available --
    and we use this non-null pointer instead.
    
    Fixes: 8c4791ae2395 ("libmlx5: First version of libmlx5")
    Fixes: ec90e3c2f8b9 ("Allocate separate RQ and SQ buffers for Raw Packet QP")
    Signed-off-by: Jack Morgenstein <jackm@dev.mellanox.co.il>
    Reviewed-by: Yishai Hadas <yishaih@mellanox.com>
    Cc: stable@linux-rdma.org # v15

--- providers/mlx5/verbs.c
+++ providers/mlx5/verbs.c
@@ -1120,7 +1120,7 @@ static int mlx5_alloc_qp_buf(struct ibv_context *context,
 
 	return 0;
 rq_buf:
-	mlx5_free_actual_buf(to_mctx(qp->verbs_qp.qp.context), &qp->buf);
+	mlx5_free_actual_buf(to_mctx(context), &qp->buf);
 ex_wrid:
 	if (qp->rq.wrid)
 		free(qp->rq.wrid);
@@ -1136,10 +1136,8 @@ ex_wrid:
 	return err;
 }
 
-static void mlx5_free_qp_buf(struct mlx5_qp *qp)
+static void mlx5_free_qp_buf(struct mlx5_context *ctx, struct mlx5_qp *qp)
 {
-	struct mlx5_context *ctx = to_mctx(qp->ibv_qp->context);
-
 	mlx5_free_actual_buf(ctx, &qp->buf);
 
 	if (qp->sq_buf.buf)
@@ -1420,7 +1418,7 @@ err_rq_db:
 	mlx5_free_db(to_mctx(context), qp->db);
 
 err_free_qp_buf:
-	mlx5_free_qp_buf(qp);
+	mlx5_free_qp_buf(ctx, qp);
 
 err:
 	free(qp);
@@ -1531,7 +1529,7 @@ int mlx5_destroy_qp(struct ibv_qp *ibqp)
 		mlx5_clear_uidx(ctx, qp->rsc.rsn);
 
 	mlx5_free_db(ctx, qp->db);
-	mlx5_free_qp_buf(qp);
+	mlx5_free_qp_buf(ctx, qp);
 free:
 	free(qp);
 
