dd01031b4cac05ab00bcc4b57ba3fb14323e1de4
Jason Gunthorpe <jgg@mellanox.com>
2018-09-13 16:33:45 -0600

    Generate complete and sanitized static libraries
    Once rdma-core started to introduce ccan and util internal libraries the
    static libraries stopped working in a sane way. We need to include the
    internal libraries within the static ones and sanitize the symbol names to
    get things back to where they used to be.
    
    Add a post-processing script that figures out what to do and invokes
    binutils to generate clean static libraries.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- buildlib/rdma_functions.cmake
+++ buildlib/rdma_functions.cmake
@@ -4,11 +4,25 @@
 # Helper functions for use in the sub CMakeLists files to make them simpler
 # and more uniform.
 
-# Global list of pairs of (SHARED STATIC) libary target names
+# Global list of tuples of (SHARED STATIC MAP) library target names
 set(RDMA_STATIC_LIBS "" CACHE INTERNAL "Doc" FORCE)
 
 set(COMMON_LIBS_PIC ccan_pic rdma_util_pic)
 set(COMMON_LIBS ccan rdma_util)
+
+function(rdma_public_static_lib SHLIB STATICLIB VERSION_SCRIPT)
+  if (NOT IS_ABSOLUTE ${VERSION_SCRIPT})
+    set(VERSION_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/${VERSION_SCRIPT}")
+  endif()
+
+  set_target_properties(${STATICLIB} PROPERTIES
+    OUTPUT_NAME ${SHLIB}
+    ARCHIVE_OUTPUT_DIRECTORY "${BUILD_STATIC_LIB}")
+  target_compile_definitions(${STATICLIB} PRIVATE _STATIC_LIBRARY_BUILD_=1)
+
+  list(APPEND RDMA_STATIC_LIBS ${SHLIB} ${STATICLIB} ${VERSION_SCRIPT})
+  set(RDMA_STATIC_LIBS "${RDMA_STATIC_LIBS}" CACHE INTERNAL "")
+endfunction()
 
 function(rdma_make_dir DDIR)
   if(NOT EXISTS "${DDIR}/")
@@ -88,14 +102,8 @@ function(rdma_library DEST VERSION_SCRIPT SOVERSION VERSION)
   # Create a static library
   if (ENABLE_STATIC)
     add_library(${DEST}-static STATIC ${ARGN})
-    set_target_properties(${DEST}-static PROPERTIES
-      OUTPUT_NAME ${DEST}
-      ARCHIVE_OUTPUT_DIRECTORY "${BUILD_LIB}")
-    target_compile_definitions(${DEST}-static PRIVATE _STATIC_LIBRARY_BUILD_=1)
-    install(TARGETS ${DEST}-static DESTINATION "${CMAKE_INSTALL_LIBDIR}")
-
-    list(APPEND RDMA_STATIC_LIBS ${DEST} ${DEST}-static)
-    set(RDMA_STATIC_LIBS "${RDMA_STATIC_LIBS}" CACHE INTERNAL "")
+    target_link_libraries(${DEST}-static LINK ${COMMON_LIBS})
+    rdma_public_static_lib(${DEST} ${DEST}-static ${VERSION_SCRIPT})
   endif()
 
   # Create a shared library
@@ -125,13 +133,7 @@ function(rdma_shared_provider DEST VERSION_SCRIPT SOVERSION VERSION)
   # Create a static provider library
   if (ENABLE_STATIC)
     add_library(${DEST}-static STATIC ${ARGN})
-    set_target_properties(${DEST}-static PROPERTIES OUTPUT_NAME ${DEST})
-    set_target_properties(${DEST}-static PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${BUILD_LIB}")
-    target_compile_definitions(${DEST}-static PRIVATE _STATIC_LIBRARY_BUILD_=1)
-    install(TARGETS ${DEST}-static DESTINATION "${CMAKE_INSTALL_LIBDIR}")
-
-    list(APPEND RDMA_STATIC_LIBS ${DEST} ${DEST}-static)
-    set(RDMA_STATIC_LIBS "${RDMA_STATIC_LIBS}" CACHE INTERNAL "")
+    rdma_public_static_lib(${DEST} ${DEST}-static ${VERSION_SCRIPT})
   endif()
 
   # Create the plugin shared library
@@ -177,12 +179,8 @@ function(rdma_provider DEST)
   # but we don't have any directions on how to make static linking work..
   if (ENABLE_STATIC)
     add_library(${DEST} STATIC ${ARGN})
-    set_target_properties(${DEST} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${BUILD_LIB}")
-    target_compile_definitions(${DEST} PRIVATE _STATIC_LIBRARY_BUILD_=1)
-    install(TARGETS ${DEST} DESTINATION "${CMAKE_INSTALL_LIBDIR}")
-
-    list(APPEND RDMA_STATIC_LIBS "${DEST}-rdmav${IBVERBS_PABI_VERSION}" ${DEST})
-    set(RDMA_STATIC_LIBS "${RDMA_STATIC_LIBS}" CACHE INTERNAL "")
+    rdma_public_static_lib("${DEST}-rdmav${IBVERBS_PABI_VERSION}" ${DEST} ${BUILDLIB}/provider.map)
+    set_target_properties(${DEST} PROPERTIES OUTPUT_NAME ${DEST})
   endif()
 
   # Create the plugin shared library
@@ -234,18 +232,20 @@ function(rdma_test_executable EXEC)
 endfunction()
 
 # Finalize the setup of the static libraries by copying the meta information
-# from the shared and setting up the libtool .la files.
+# from the shared to static and setting up the static builder
 function(rdma_finalize_libs)
   list(LENGTH RDMA_STATIC_LIBS LEN)
-  if (LEN LESS 2)
+  if (LEN LESS 3)
     return()
   endif()
 
   math(EXPR LEN ${LEN}-1)
-  foreach(I RANGE 0 ${LEN} 2)
+  foreach(I RANGE 0 ${LEN} 3)
     list(GET RDMA_STATIC_LIBS ${I} SHARED)
     math(EXPR I ${I}+1)
     list(GET RDMA_STATIC_LIBS ${I} STATIC)
+    math(EXPR I ${I}+1)
+    list(GET RDMA_STATIC_LIBS ${I} MAP)
 
     # PUBLIC libraries
     set(LIBS "")
@@ -263,7 +263,29 @@ function(rdma_finalize_libs)
       set_target_properties(${STATIC} PROPERTIES LINK_LIBRARIES "${TMP}")
       list(APPEND LIBS "${TMP}")
     endif()
+
+    set(ARGS ${ARGS} --map "${MAP}" --lib "$<TARGET_FILE:${STATIC}>")
+    set(DEPENDS ${DEPENDS} ${STATIC} ${MAP})
+
+    get_target_property(TMP ${STATIC} OUTPUT_NAME)
+    set(OUTPUTS ${OUTPUTS} "${BUILD_LIB}/lib${TMP}.a")
+    install(FILES "${BUILD_LIB}/lib${TMP}.a" DESTINATION "${CMAKE_INSTALL_LIBDIR}")
   endforeach()
+
+  foreach(STATIC ${COMMON_LIBS})
+    set(ARGS ${ARGS} --internal_lib "$<TARGET_FILE:${STATIC}>")
+    set(DEPENDS ${DEPENDS} ${STATIC})
+  endforeach()
+
+  add_custom_command(
+    OUTPUT ${OUTPUTS}
+    COMMAND "${PYTHON_EXECUTABLE}" "${CMAKE_SOURCE_DIR}/buildlib/sanitize_static_lib.py"
+             --version ${PACKAGE_VERSION}
+             --ar "${CMAKE_AR}" --nm "${CMAKE_NM}" --objcopy "${CMAKE_OBJCOPY}" ${ARGS}
+    DEPENDS ${DEPENDS} "${CMAKE_SOURCE_DIR}/buildlib/sanitize_static_lib.py"
+    COMMENT "Building distributable static libraries"
+    VERBATIM)
+  add_custom_target("make_static" ALL DEPENDS ${OUTPUTS})
 endfunction()
 
 # Generate a pkg-config file
