47eff6e8624d1b8af41ccd64371d587d1a3199b5
Lang Cheng <chenglang@huawei.com>
2019-05-24 17:37:48 +0800

    libhns: Adjust the order of parameter checking
    We should check bind_info->mr before bind_info->mr->pd.
    
    Signed-off-by: Lang Cheng <chenglang@huawei.com>

--- providers/hns/hns_roce_u_verbs.c
+++ providers/hns/hns_roce_u_verbs.c
@@ -183,13 +183,13 @@ int hns_roce_u_bind_mw(struct ibv_qp *qp, struct ibv_mw *mw,
 	struct ibv_send_wr wr = {};
 	int ret;
 
+	if (!bind_info->mr && bind_info->length)
+		return EINVAL;
+
 	if ((mw->pd != qp->pd) || (mw->pd != bind_info->mr->pd))
 		return EINVAL;
 
 	if (mw->type != IBV_MW_TYPE_1)
-		return EINVAL;
-
-	if (!bind_info->mr && bind_info->length)
 		return EINVAL;
 
 	if (bind_info->mw_access_flags & ~(IBV_ACCESS_REMOTE_WRITE |
