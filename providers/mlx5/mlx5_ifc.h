83ad347a17b131b87c05952bfdea43e56d0ebcee
Yevgeny Kliteynik <kliteyn@mellanox.com>
2019-09-15 11:05:31 +0300

    mlx5: Add HW bits and definitions for Geneve flex parser
    Add definition for flex parser tunneling header for Geneve.
    
    Signed-off-by: Yevgeny Kliteynik <kliteyn@mellanox.com>
    Reviewed-by: Alex Vesker <valex@mellanox.com>
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>

--- providers/mlx5/mlx5_ifc.h
+++ providers/mlx5/mlx5_ifc.h
@@ -502,6 +502,7 @@ struct mlx5_ifc_flow_table_prop_layout_bits {
 };
 
 enum {
+	MLX5_FLEX_PARSER_GENEVE_ENABLED		= 1 << 3,
 	MLX5_FLEX_PARSER_VXLAN_GPE_ENABLED	= 1 << 7,
 	MLX5_FLEX_PARSER_ICMP_V4_ENABLED	= 1 << 8,
 	MLX5_FLEX_PARSER_ICMP_V6_ENABLED	= 1 << 9,
@@ -1603,6 +1604,19 @@ struct mlx5_ifc_ste_flex_parser_tnl_vxlan_gpe_bits {
 	u8         outer_vxlan_gpe_next_protocol[0x8];
 
 	u8         outer_vxlan_gpe_vni[0x18];
+	u8         reserved_at_38[0x8];
+
+	u8         reserved_at_40[0x40];
+};
+
+struct mlx5_ifc_ste_flex_parser_tnl_geneve_bits {
+	u8         reserved_at_0[0x2];
+	u8         geneve_opt_len[0x6];
+	u8         geneve_oam[0x1];
+	u8         reserved_at_9[0x7];
+	u8         geneve_protocol_type[0x10];
+
+	u8         geneve_vni[0x18];
 	u8         reserved_at_38[0x8];
 
 	u8         reserved_at_40[0x40];
