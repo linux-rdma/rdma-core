182222edddd777a0d0dd856b9791c0fd309ab82d
Noa Osherovich <noaos@mellanox.com>
2019-10-28 14:36:16 +0200

    tests: Make unittest command line arguments work
    This fixes the test loader so that the normal -k and 'tests.test_foo'
    arguments work for the test loader. This lets the user run a subset of
    tests directly from the command line, as expected from unittest.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,13 +7,6 @@ import os
 from importlib.machinery import SourceFileLoader
 
 
-def test_all():
-    module_path = os.path.dirname(__file__) + '/__init__.py'
-    module = SourceFileLoader('tests', module_path).load_module()
-    loader = unittest.TestLoader()
-    suite = loader.loadTestsFromModule(module)
-    return suite
-
-
-if __name__ == '__main__':
-    unittest.main(defaultTest="test_all")
+module_path = os.path.dirname(__file__) + '/__init__.py'
+tests = SourceFileLoader('tests', module_path).load_module()
+unittest.main(module=tests)
