a1be0c0c132da2ddf1f7b244120ee1db1c0b66af
Shadi Ammouri <sammouri@amazon.com>
2020-01-07 14:20:21 +0000

    pyverbs/efa: Support printing device caps in human readable format
    Convert and print direct verb's device_caps bitmask in human readable
    format.
    For now, we print device_caps in raw and textual formats.
    
    Signed-off-by: Shadi Ammouri <sammouri@amazon.com>
    Co-developed-by: Gal Pressman <galpress@amazon.com>
    Signed-off-by: Gal Pressman <galpress@amazon.com>

--- pyverbs/providers/efa/efadv.pyx
+++ pyverbs/providers/efa/efadv.pyx
@@ -1,9 +1,29 @@
 # SPDX-License-Identifier: (GPL-2.0 OR Linux-OpenIB)
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All rights reserved.
 
+cimport pyverbs.providers.efa.efadv_enums as dve
 cimport pyverbs.providers.efa.libefa as dv
 
 from pyverbs.base import PyverbsRDMAError
+
+
+def dev_cap_to_str(flags):
+    l = {dve.EFADV_DEVICE_ATTR_CAPS_RDMA_READ: 'RDMA Read'}
+    return bitmask_to_str(flags, l)
+
+
+def bitmask_to_str(bits, values):
+    numeric_bits = bits
+    flags = []
+    for k, v in sorted(values.items()):
+        if bits & k:
+            flags.append(v)
+            bits -= k
+    if bits:
+        flags.append(f'??({bits:x})')
+    if not flags:
+        flags.append('None')
+    return ', '.join(flags) + f' ({numeric_bits:x})'
 
 
 cdef class EfaContext(Context):
@@ -75,5 +95,5 @@ cdef class EfaDVDeviceAttr(PyverbsObject):
             print_format.format('Max SQ SQE', self.dv.max_sq_sge) + \
             print_format.format('Max RQ SQE', self.dv.max_rq_sge) + \
             print_format.format('Inline buffer size', self.dv.inline_buf_size) + \
-            print_format.format('Device Capabilities', self.dv.device_caps) + \
+            print_format.format('Device Capabilities', dev_cap_to_str(self.dv.device_caps)) + \
             print_format.format('Max RDMA Size', self.dv.max_rdma_size)
