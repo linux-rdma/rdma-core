linux-rdma/rdma-core

commit c28410765bdfe5cbed3cb2cdb1584eac3941469c

libibverbs: Fix incorrect return code
The proper return code is EOPNOTSUPP when an operation is not supported.

Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

libibverbs/dummy_ops.c
@@ -39,25 +39,25 @@ static int advise_mr(struct ibv_pd *pd,
 		     struct ibv_sge *sg_list,
 		     uint32_t num_sges)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static struct ibv_dm *alloc_dm(struct ibv_context *context,
 			       struct ibv_alloc_dm_attr *attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_mw *alloc_mw(struct ibv_pd *pd, enum ibv_mw_type type)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_mr *alloc_null_mr(struct ibv_pd *pd)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
@@ -65,20 +65,20 @@ static struct ibv_pd *
 alloc_parent_domain(struct ibv_context *context,
 		    struct ibv_parent_domain_init_attr *attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_pd *alloc_pd(struct ibv_context *context)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_td *alloc_td(struct ibv_context *context,
 			       struct ibv_td_init_attr *init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
@@ -91,24 +91,24 @@ static int attach_counters_point_flow(struct ibv_counters *counters,
 				      struct ibv_counter_attach_attr *attr,
 				      struct ibv_flow *flow)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int attach_mcast(struct ibv_qp *qp, const union ibv_gid *gid,
 			uint16_t lid)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int bind_mw(struct ibv_qp *qp, struct ibv_mw *mw,
 		   struct ibv_mw_bind *mw_bind)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int close_xrcd(struct ibv_xrcd *xrcd)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static void cq_event(struct ibv_cq *cq)
@@ -117,14 +117,14 @@ static void cq_event(struct ibv_cq *cq)
 
 static struct ibv_ah *create_ah(struct ibv_pd *pd, struct ibv_ah_attr *attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_counters *create_counters(struct ibv_context *context,
 					    struct ibv_counters_init_attr *init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
@@ -132,42 +132,42 @@ static struct ibv_cq *create_cq(struct ibv_context *context, int cqe,
 				struct ibv_comp_channel *channel,
 				int comp_vector)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_cq_ex *create_cq_ex(struct ibv_context *context,
 				      struct ibv_cq_init_attr_ex *init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_flow *create_flow(struct ibv_qp *qp,
 				    struct ibv_flow_attr *flow_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_flow_action *create_flow_action_esp(struct ibv_context *context,
 						      struct ibv_flow_action_esp_attr *attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_qp *create_qp(struct ibv_pd *pd,
 				struct ibv_qp_init_attr *attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_qp *create_qp_ex(struct ibv_context *context,
 				   struct ibv_qp_init_attr_ex *qp_init_attr_ex)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
@@ -175,14 +175,14 @@ static struct ibv_rwq_ind_table *
 create_rwq_ind_table(struct ibv_context *context,
 		     struct ibv_rwq_ind_table_init_attr *init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_srq *create_srq(struct ibv_pd *pd,
 				  struct ibv_srq_init_attr *srq_init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
@@ -190,208 +190,208 @@ static struct ibv_srq *
 create_srq_ex(struct ibv_context *context,
 	      struct ibv_srq_init_attr_ex *srq_init_attr_ex)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_wq *create_wq(struct ibv_context *context,
 				struct ibv_wq_init_attr *wq_init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static int dealloc_mw(struct ibv_mw *mw)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int dealloc_pd(struct ibv_pd *pd)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int dealloc_td(struct ibv_td *td)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int dereg_mr(struct verbs_mr *vmr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_ah(struct ibv_ah *ah)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_counters(struct ibv_counters *counters)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_cq(struct ibv_cq *cq)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_flow(struct ibv_flow *flow)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_flow_action(struct ibv_flow_action *action)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_qp(struct ibv_qp *qp)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_rwq_ind_table(struct ibv_rwq_ind_table *rwq_ind_table)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_srq(struct ibv_srq *srq)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int destroy_wq(struct ibv_wq *wq)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int detach_mcast(struct ibv_qp *qp, const union ibv_gid *gid,
 			uint16_t lid)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int free_dm(struct ibv_dm *dm)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int get_srq_num(struct ibv_srq *srq, uint32_t *srq_num)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int modify_cq(struct ibv_cq *cq, struct ibv_modify_cq_attr *attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int modify_flow_action_esp(struct ibv_flow_action *action,
 				  struct ibv_flow_action_esp_attr *attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int modify_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr, int attr_mask)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int modify_qp_rate_limit(struct ibv_qp *qp,
 				struct ibv_qp_rate_limit_attr *attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int modify_srq(struct ibv_srq *srq, struct ibv_srq_attr *srq_attr,
 		      int srq_attr_mask)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int modify_wq(struct ibv_wq *wq, struct ibv_wq_attr *wq_attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static struct ibv_qp *open_qp(struct ibv_context *context,
 			      struct ibv_qp_open_attr *attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_xrcd *open_xrcd(struct ibv_context *context,
 				  struct ibv_xrcd_init_attr *xrcd_init_attr)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static int poll_cq(struct ibv_cq *cq, int num_entries, struct ibv_wc *wc)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int post_recv(struct ibv_qp *qp, struct ibv_recv_wr *wr,
 		     struct ibv_recv_wr **bad_wr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int post_send(struct ibv_qp *qp, struct ibv_send_wr *wr,
 		     struct ibv_send_wr **bad_wr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int post_srq_ops(struct ibv_srq *srq, struct ibv_ops_wr *op,
 			struct ibv_ops_wr **bad_op)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int post_srq_recv(struct ibv_srq *srq, struct ibv_recv_wr *recv_wr,
 			 struct ibv_recv_wr **bad_recv_wr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int query_device(struct ibv_context *context,
 			struct ibv_device_attr *device_attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int query_device_ex(struct ibv_context *context,
 			   const struct ibv_query_device_ex_input *input,
 			   struct ibv_device_attr_ex *attr, size_t attr_size)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int query_port(struct ibv_context *context, uint8_t port_num,
 		      struct ibv_port_attr *port_attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int query_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr, int attr_mask,
 		    struct ibv_qp_init_attr *init_attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int query_rt_values(struct ibv_context *context,
 			   struct ibv_values_ex *values)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int query_srq(struct ibv_srq *srq, struct ibv_srq_attr *srq_attr)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int read_counters(struct ibv_counters *counters,
@@ -399,43 +399,43 @@ static int read_counters(struct ibv_counters *counters,
 			 uint32_t ncounters,
 			 uint32_t flags)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static struct ibv_mr *reg_dm_mr(struct ibv_pd *pd, struct ibv_dm *dm,
 				uint64_t dm_offset, size_t length,
 				unsigned int access)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static struct ibv_mr *reg_mr(struct ibv_pd *pd, void *addr, size_t length,
 			     uint64_t hca_va,  int access)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return NULL;
 }
 
 static int req_notify_cq(struct ibv_cq *cq, int solicited_only)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static int rereg_mr(struct verbs_mr *vmr, int flags, struct ibv_pd *pd,
 		    void *addr, size_t length, int access)
 {
-	errno = ENOSYS;
+	errno = EOPNOTSUPP;
 	return IBV_REREG_MR_ERR_INPUT;
 }
 
 static int resize_cq(struct ibv_cq *cq, int cqe)
 {
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 /*
- * Ops in verbs_dummy_ops simply return an ENOSYS error code when called, or
+ * Ops in verbs_dummy_ops simply return an EOPNOTSUPP error code when called, or
  * do nothing. They are placed in the ops structures if the provider does not
  * provide an op for the function.
  *

libibverbs/verbs.h
@@ -2305,7 +2305,7 @@ static inline struct ibv_flow *ibv_create_flow(struct ibv_qp *qp,
 	struct verbs_context *vctx = verbs_get_ctx_op(qp->context,
 						      ibv_create_flow);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2317,7 +2317,7 @@ static inline int ibv_destroy_flow(struct ibv_flow *flow_id)
 	struct verbs_context *vctx = verbs_get_ctx_op(flow_id->context,
 						      ibv_destroy_flow);
 	if (!vctx)
-		return -ENOSYS;
+		return -EOPNOTSUPP;
 	return vctx->ibv_destroy_flow(flow_id);
 }
 
@@ -2329,7 +2329,7 @@ ibv_create_flow_action_esp(struct ibv_context *ctx,
 						      create_flow_action_esp);
 
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2344,7 +2344,7 @@ ibv_modify_flow_action_esp(struct ibv_flow_action *action,
 						      modify_flow_action_esp);
 
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->modify_flow_action_esp(action, esp);
 }
@@ -2355,7 +2355,7 @@ static inline int ibv_destroy_flow_action(struct ibv_flow_action *action)
 						      destroy_flow_action);
 
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->destroy_flow_action(action);
 }
@@ -2368,7 +2368,7 @@ ibv_open_xrcd(struct ibv_context *context, struct ibv_xrcd_init_attr *xrcd_init_
 {
 	struct verbs_context *vctx = verbs_get_ctx_op(context, open_xrcd);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 	return vctx->open_xrcd(context, xrcd_init_attr);
@@ -2430,7 +2430,7 @@ static inline struct ibv_mw *ibv_alloc_mw(struct ibv_pd *pd,
 	struct ibv_mw *mw;
 
 	if (!pd->context->ops.alloc_mw) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2497,7 +2497,7 @@ static inline int ibv_advise_mr(struct ibv_pd *pd,
 
 	vctx = verbs_get_ctx_op(pd->context, advise_mr);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->advise_mr(pd, advice, flags, sg_list, num_sge);
 }
@@ -2514,7 +2514,7 @@ struct ibv_dm *ibv_alloc_dm(struct ibv_context *context,
 	struct verbs_context *vctx = verbs_get_ctx_op(context, alloc_dm);
 
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2531,7 +2531,7 @@ int ibv_free_dm(struct ibv_dm *dm)
 	struct verbs_context *vctx = verbs_get_ctx_op(dm->context, free_dm);
 
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->free_dm(dm);
 }
@@ -2568,7 +2568,7 @@ struct ibv_mr *ibv_alloc_null_mr(struct ibv_pd *pd)
 
 	vctx = verbs_get_ctx_op(pd->context, alloc_null_mr);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2591,7 +2591,7 @@ struct ibv_mr *ibv_reg_dm_mr(struct ibv_pd *pd, struct ibv_dm *dm,
 	struct verbs_context *vctx = verbs_get_ctx_op(pd->context, reg_dm_mr);
 
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2625,7 +2625,7 @@ struct ibv_cq_ex *ibv_create_cq_ex(struct ibv_context *context,
 	struct verbs_context *vctx = verbs_get_ctx_op(context, create_cq_ex);
 
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2710,7 +2710,7 @@ static inline int ibv_modify_cq(struct ibv_cq *cq, struct ibv_modify_cq_attr *at
 	struct verbs_context *vctx = verbs_get_ctx_op(cq->context, modify_cq);
 
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->modify_cq(cq, attr);
 }
@@ -2744,7 +2744,7 @@ ibv_create_srq_ex(struct ibv_context *context,
 
 	vctx = verbs_get_ctx_op(context, create_srq_ex);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 	return vctx->create_srq_ex(context, srq_init_attr_ex);
@@ -2779,7 +2779,7 @@ static inline int ibv_get_srq_num(struct ibv_srq *srq, uint32_t *srq_num)
 	struct verbs_context *vctx = verbs_get_ctx_op(srq->context, get_srq_num);
 
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->get_srq_num(srq, srq_num);
 }
@@ -2813,7 +2813,7 @@ static inline int ibv_post_srq_ops(struct ibv_srq *srq,
 	vctx = verbs_get_ctx_op(srq->context, post_srq_ops);
 	if (!vctx) {
 		*bad_op = op;
-		return ENOSYS;
+		return EOPNOTSUPP;
 	}
 	return vctx->post_srq_ops(srq, op, bad_op);
 }
@@ -2836,7 +2836,7 @@ ibv_create_qp_ex(struct ibv_context *context, struct ibv_qp_init_attr_ex *qp_ini
 
 	vctx = verbs_get_ctx_op(context, create_qp_ex);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 	return vctx->create_qp_ex(context, qp_init_attr_ex);
@@ -2852,7 +2852,7 @@ static inline struct ibv_td *ibv_alloc_td(struct ibv_context *context,
 
 	vctx = verbs_get_ctx_op(context, alloc_td);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2868,7 +2868,7 @@ static inline int ibv_dealloc_td(struct ibv_td *td)
 
 	vctx = verbs_get_ctx_op(td->context, dealloc_td);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->dealloc_td(td);
 }
@@ -2884,7 +2884,7 @@ ibv_alloc_parent_domain(struct ibv_context *context,
 
 	vctx = verbs_get_ctx_op(context, alloc_parent_domain);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -2904,7 +2904,7 @@ ibv_query_rt_values_ex(struct ibv_context *context,
 
 	vctx = verbs_get_ctx_op(context, query_rt_values);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->query_rt_values(context, values);
 }
@@ -2925,7 +2925,7 @@ ibv_query_device_ex(struct ibv_context *context,
 		goto legacy;
 
 	ret = vctx->query_device_ex(context, input, attr, sizeof(*attr));
-	if (ret == ENOSYS)
+	if (ret == EOPNOTSUPP)
 		goto legacy;
 
 	return ret;
@@ -2945,7 +2945,7 @@ ibv_open_qp(struct ibv_context *context, struct ibv_qp_open_attr *qp_open_attr)
 {
 	struct verbs_context *vctx = verbs_get_ctx_op(context, open_qp);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 	return vctx->open_qp(context, qp_open_attr);
@@ -2970,7 +2970,7 @@ ibv_modify_qp_rate_limit(struct ibv_qp *qp,
 
 	vctx = verbs_get_ctx_op(qp->context, modify_qp_rate_limit);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->modify_qp_rate_limit(qp, attr);
 }
@@ -3020,7 +3020,7 @@ static inline struct ibv_wq *ibv_create_wq(struct ibv_context *context,
 	struct ibv_wq *wq;
 
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -3052,7 +3052,7 @@ static inline int ibv_modify_wq(struct ibv_wq *wq, struct ibv_wq_attr *wq_attr)
 	struct verbs_context *vctx = verbs_get_ctx_op(wq->context, modify_wq);
 
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->modify_wq(wq, wq_attr);
 }
@@ -3070,7 +3070,7 @@ static inline int ibv_destroy_wq(struct ibv_wq *wq)
 
 	vctx = verbs_get_ctx_op(wq->context, destroy_wq);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->destroy_wq(wq);
 }
@@ -3090,7 +3090,7 @@ static inline struct ibv_rwq_ind_table *ibv_create_rwq_ind_table(struct ibv_cont
 
 	vctx = verbs_get_ctx_op(context, create_rwq_ind_table);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -3110,7 +3110,7 @@ static inline int ibv_destroy_rwq_ind_table(struct ibv_rwq_ind_table *rwq_ind_ta
 
 	vctx = verbs_get_ctx_op(rwq_ind_table->context, destroy_rwq_ind_table);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->destroy_rwq_ind_table(rwq_ind_table);
 }
@@ -3238,7 +3238,7 @@ static inline struct ibv_counters *ibv_create_counters(struct ibv_context *conte
 
 	vctx = verbs_get_ctx_op(context, create_counters);
 	if (!vctx) {
-		errno = ENOSYS;
+		errno = EOPNOTSUPP;
 		return NULL;
 	}
 
@@ -3251,7 +3251,7 @@ static inline int ibv_destroy_counters(struct ibv_counters *counters)
 
 	vctx = verbs_get_ctx_op(counters->context, destroy_counters);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->destroy_counters(counters);
 }
@@ -3264,7 +3264,7 @@ static inline int ibv_attach_counters_point_flow(struct ibv_counters *counters,
 
 	vctx = verbs_get_ctx_op(counters->context, attach_counters_point_flow);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->attach_counters_point_flow(counters, attr, flow);
 }
@@ -3278,7 +3278,7 @@ static inline int ibv_read_counters(struct ibv_counters *counters,
 
 	vctx = verbs_get_ctx_op(counters->context, read_counters);
 	if (!vctx)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	return vctx->read_counters(counters, counters_value, ncounters, flags);
 }

libibverbs/cmd.c
@@ -303,7 +303,7 @@ int ibv_cmd_open_xrcd(struct ibv_context *context, struct verbs_xrcd *xrcd,
 	int ret;
 
 	if (attr->comp_mask >= IBV_XRCD_INIT_ATTR_RESERVED)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	if (!(attr->comp_mask & IBV_XRCD_INIT_ATTR_FD) ||
 	    !(attr->comp_mask & IBV_XRCD_INIT_ATTR_OFLAGS))
@@ -544,7 +544,7 @@ int ibv_cmd_create_srq_ex(struct ibv_context *context,
 	int ret;
 
 	if (attr_ex->comp_mask >= IBV_SRQ_INIT_ATTR_RESERVED)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	if (!(attr_ex->comp_mask & IBV_SRQ_INIT_ATTR_PD))
 		return EINVAL;
@@ -863,7 +863,7 @@ int ibv_cmd_create_qp_ex(struct ibv_context *context,
 			     IBV_QP_INIT_ATTR_PD |
 			     IBV_QP_INIT_ATTR_XRCD |
 			     IBV_QP_INIT_ATTR_SEND_OPS_FLAGS))
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	err = create_qp_ex_common(qp, attr_ex, vxrcd,
 				  &cmd->core_payload);
@@ -964,7 +964,7 @@ int ibv_cmd_open_qp(struct ibv_context *context, struct verbs_qp *qp,
 	int ret;
 
 	if (attr->comp_mask >= IBV_QP_OPEN_ATTR_RESERVED)
-		return ENOSYS;
+		return EOPNOTSUPP;
 
 	if (!(attr->comp_mask & IBV_QP_OPEN_ATTR_XRCD) ||
 	    !(attr->comp_mask & IBV_QP_OPEN_ATTR_NUM) ||

libibverbs/driver.h
@@ -639,7 +639,7 @@ static inline int verbs_get_srq_num(struct ibv_srq *srq, uint32_t *srq_num)
 		*srq_num = vsrq->srq_num;
 		return 0;
 	}
-	return ENOSYS;
+	return EOPNOTSUPP;
 }
 
 static inline bool check_comp_mask(uint64_t input, uint64_t supported)

libibverbs/man/ibv_modify_qp_rate_limit.3
@@ -52,8 +52,9 @@ returns 0 on success, or the value of errno on failure (which indicates the fail
 .SH "ERRORS"
 .SS EINVAL
 Invalid arguments.
-.SS ENOSYS
+.SS EOPNOTSUPP
 Function is not implemented for this device.
+(ENOSYS may sometimes be returned by old versions of libibverbs).
 .PP
 .SH "SEE ALSO"
 .BR ibv_create_qp (3),

commit 58ccb638b540b41d2870ce252d6795443e199fe9

libibverbs: Fix query_device_ex dummy function not to return EOPNOTSUPP
Implement query_device_ex dummy function for all providers.
Returning EOPNOTSUPP broke compatibility with apps compiled against
older rdma-core

Suggested-by: Jason Gunthorpe <jgg@mellanox.com>
Cc: stable@linux-rdma.org # v28
Fixes: commit c28410765bdf ("libibverbs: Fix incorrect return code")
Signed-off-by: Michal Kalderon <michal.kalderon@marvell.com>

libibverbs/dummy_ops.c
@@ -369,11 +369,22 @@ static int query_device(struct ibv_context *context,
 	return EOPNOTSUPP;
 }
 
+/* Provide a generic implementation for all providers that don't implement
+ * query_device_ex.
+ */
 static int query_device_ex(struct ibv_context *context,
 			   const struct ibv_query_device_ex_input *input,
 			   struct ibv_device_attr_ex *attr, size_t attr_size)
 {
-	return EOPNOTSUPP;
+	if (input && input->comp_mask)
+		return EINVAL;
+
+	if (attr_size < sizeof(attr->orig_attr))
+		return EOPNOTSUPP;
+
+	memset(&attr->orig_attr, 0, sizeof(attr->orig_attr));
+
+	return ibv_query_device(context, &attr->orig_attr);
 }
 
 static int query_port(struct ibv_context *context, uint8_t port_num,

libibverbs/man/ibv_query_device_ex.3
@@ -3,18 +3,23 @@
 .\"
 .TH IBV_QUERY_DEVICE_EX 3 2014-12-17 libibverbs "Libibverbs Programmer's Manual"
 .SH "NAME"
-ibv_query_device_ex \- query an RDMA device's attributes
+ibv_query_device_ex \- query an RDMA device's attributes including extended
+device properties.
 .SH "SYNOPSIS"
 .nf
 .B #include <infiniband/verbs.h>
 .sp
 .BI "int ibv_query_device_ex(struct ibv_context " "*context",
+.BI "                        struct ibv_query_device_ex_input " "*input",
 .BI "                        struct ibv_device_attr_ex " "*attr" );
 .fi
 .SH "DESCRIPTION"
 .B ibv_query_device_ex()
 returns the attributes of the device with context
 .I context\fR.
+The argument
+.I input
+is a pointer to an ibv_query_device_ex_input structure, used for future extensions
 The argument
 .I attr
 is a pointer to an ibv_device_attr_ex struct, as defined in <infiniband/verbs.h>.

libibverbs/verbs.h
@@ -2962,12 +2962,15 @@ ibv_query_device_ex(struct ibv_context *context,
 	struct verbs_context *vctx;
 	int ret;
 
+	if (input && input->comp_mask)
+		return EINVAL;
+
 	vctx = verbs_get_ctx_op(context, query_device_ex);
 	if (!vctx)
 		goto legacy;
 
 	ret = vctx->query_device_ex(context, input, attr, sizeof(*attr));
-	if (ret == EOPNOTSUPP)
+	if (ret == EOPNOTSUPP || ret == ENOSYS)
 		goto legacy;
 
 	return ret;