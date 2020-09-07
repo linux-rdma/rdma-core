8bb38f6cb1b29e7c7dc36302245d51903167347d
Honggang Li <honli@redhat.com>
2020-09-07 20:42:31 +0800

    redhat: update dracut setting
    Signed-off-by: Honggang Li <honli@redhat.com>

--- redhat/rdma.modules-setup.sh
+++ redhat/rdma.modules-setup.sh
@@ -12,11 +12,20 @@ depends() {
 
 install() {
 	inst /etc/rdma/mlx4.conf
+	inst /etc/rdma/modules/infiniband.conf
+	inst /etc/rdma/modules/iwarp.conf
+	inst /etc/rdma/modules/opa.conf
+	inst /etc/rdma/modules/rdma.conf
+	inst /etc/rdma/modules/roce.conf
 	inst /usr/libexec/mlx4-setup.sh
 	inst /usr/lib/modprobe.d/libmlx4.conf
 	inst_multiple lspci setpci awk sleep
 	inst_multiple -o /etc/modprobe.d/mlx4.conf
-	inst_rules 70-persistent-ipoib.rules
+	inst_rules 60-rdma-ndd.rules 60-rdma-persistent-naming.rules 70-persistent-ipoib.rules 75-rdma-description.rules 90-rdma-hw-modules.rules 90-rdma-ulp-modules.rules 90-rdma-umad.rules
+	inst_multiple -o \
+                  $systemdsystemunitdir/rdma-hw.target \
+                  $systemdsystemunitdir/rdma-load-modules@.service \
+                  $systemdsystemunitdir/rdma-ndd.service
 }
 
 installkernel() {
