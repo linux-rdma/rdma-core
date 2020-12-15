9f014b0049bca7b5732f89e9ebd61fd042a54458
Daria Velikovsky <daria@nvidia.com>
2020-12-15 18:57:46 +0200

    tests: Add tests for mlx5dv_create_flow
    Add a test for creating a flow that matches on source mac with action
    QP.
    
    Signed-off-by: Daria Velikovsky <daria@nvidia.com>
    Signed-off-by: Edward Srouji <edwards@nvidia.com>

--- tests/test_mlx5_flow.py
+++ tests/test_mlx5_flow.py
@@ -8,10 +8,13 @@ import unittest
 import errno
 
 from pyverbs.providers.mlx5.mlx5dv_flow import Mlx5FlowMatcher, \
-    Mlx5FlowMatcherAttr, Mlx5FlowMatchParameters
+    Mlx5FlowMatcherAttr, Mlx5FlowMatchParameters, Mlx5FlowActionAttr, Mlx5Flow
+from tests.utils import requires_root_on_eth, PacketConsts
 from pyverbs.pyverbs_error import PyverbsRDMAError
 from tests.base import RDMATestCase, RawResources
+import pyverbs.providers.mlx5.mlx5_enums as dve
 import tests.utils as u
+import struct
 
 
 MAX_MATCH_PARAM_SIZE = 0x180
@@ -39,6 +42,10 @@ class Mlx5FlowResources(RawResources):
             raise ex
         return matcher
 
+    @requires_root_on_eth()
+    def create_qps(self):
+        super().create_qps()
+
 
 class Mlx5MatcherTest(RDMATestCase):
     def setUp(self):
@@ -46,6 +53,17 @@ class Mlx5MatcherTest(RDMATestCase):
         self.iters = 10
         self.server = None
         self.client = None
+
+    def create_players(self, resource, **resource_arg):
+        """
+        Init Flow tests resources.
+        :param resource: The RDMA resources to use.
+        :param resource_arg: Dict of args that specify the resource specific
+                             attributes.
+        :return: None
+        """
+        self.client = resource(**self.dev_info, **resource_arg)
+        self.server = resource(**self.dev_info, **resource_arg)
 
     @u.skip_unsupported
     def test_create_empty_matcher(self):
@@ -64,3 +82,21 @@ class Mlx5MatcherTest(RDMATestCase):
         self.res = Mlx5FlowResources(**self.dev_info)
         smac_mask = bytes([0xff, 0xff, 0xff, 0xff, 0xff, 0xff])
         self.res.create_matcher(smac_mask, u.MatchCriteriaEnable.OUTER)
+
+    @u.skip_unsupported
+    def test_smac_matcher_to_qp_flow(self):
+        """
+        Creates a matcher to match on outer source mac and a flow that forwards
+        packets to QP when matching on source mac.
+        """
+        self.create_players(Mlx5FlowResources)
+        smac_mask = bytes([0xff] * 6)
+        matcher = self.server.create_matcher(smac_mask,
+                                             u.MatchCriteriaEnable.OUTER)
+        smac_value = struct.pack('!6s',
+                                 bytes.fromhex(PacketConsts.SRC_MAC.replace(':', '')))
+        value_param = Mlx5FlowMatchParameters(len(smac_value), smac_value)
+        action_qp = Mlx5FlowActionAttr(action_type=dve.MLX5DV_FLOW_ACTION_DEST_IBV_QP,
+                                       qp=self.server.qp)
+        self.server.flow = Mlx5Flow(matcher, value_param, [action_qp], 1)
+        u.raw_traffic(self.client, self.server, self.iters)
