a4ecc144ce56e2aae3e95cc0f33b6cf4eb8b8f5f
Kamal Heib <kamalheib1@gmail.com>
2020-12-27 00:13:25 +0200

    tests: Add an option to run tests on the device port
    Add "--port" command line argument that will be used to run the tests on
    a specific device port.
    
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- tests/args_parser.py
+++ tests/args_parser.py
@@ -16,6 +16,9 @@ class ArgsParser(object):
         parser = argparse.ArgumentParser()
         parser.add_argument('--dev',
                             help='RDMA device to run the tests on')
+        parser.add_argument('--port',
+                            help='Use port <port> of RDMA device', type=int,
+                            default=1)
         parser.add_argument('-v', '--verbose', dest='verbosity',
                             action='store_const',
                             const=2, help='Verbose output')
