c61d78e31860df2168923a859738d6fc3427e549
Wenpeng Liang <liangwenpeng@huawei.com>
2021-11-24 19:03:54 +0800

    libhns: hr ilog32() should be represented by a function instead of a macro
    [ Upstream commit 3cf1fd8933d6c87e64de44fe5175a22c109f6dd2 ]
    
    The compiler will check whether the modifiers of the function are of the
    correct type.
    
    Signed-off-by: Wenpeng Liang <liangwenpeng@huawei.com>
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- providers/hns/hns_roce_u.h
+++ providers/hns/hns_roce_u.h
@@ -263,6 +263,11 @@ struct hns_roce_u_hw {
 	struct verbs_context_ops hw_ops;
 };
 
+static inline unsigned int hr_ilog32(unsigned int count)
+{
+	return ilog32(count - 1);
+}
+
 static inline struct hns_roce_device *to_hr_dev(struct ibv_device *ibv_dev)
 {
 	return container_of(ibv_dev, struct hns_roce_device, ibv_dev.device);
