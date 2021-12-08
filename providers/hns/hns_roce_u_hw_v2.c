a9d866cd4671414065970be82f7341d33e1a95b8
Lang Cheng <chenglang@huawei.com>
2021-12-08 19:03:56 +0800

    libhns: Forcibly rewrite the strong-order flag of WQE
    [ Upstream commit 11495c03ef9fa30318533f99607508b88cbe32ea ]
    
    The Local Invalid operation sets so flag, otherwise clears so flag.
    
    Fixes: a9ae7e9bfb5d ("libhns: Add local invalidate MR support for hip08")
    Signed-off-by: Lang Cheng <chenglang@huawei.com>
    Signed-off-by: Wenpeng Liang <liangwenpeng@huawei.com>
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- providers/hns/hns_roce_u_hw_v2.c
+++ providers/hns/hns_roce_u_hw_v2.c
@@ -1070,10 +1070,6 @@ static int set_rc_wqe(void *wqe, struct hns_roce_qp *qp, struct ibv_send_wr *wr,
 
 	memset(rc_sq_wqe, 0, sizeof(struct hns_roce_rc_sq_wqe));
 
-	ret = check_rc_opcode(rc_sq_wqe, wr);
-	if (ret)
-		return ret;
-
 	roce_set_bit(rc_sq_wqe->byte_4, RC_SQ_WQE_BYTE_4_CQE_S,
 		     !!(wr->send_flags & IBV_SEND_SIGNALED));
 	roce_set_bit(rc_sq_wqe->byte_4, RC_SQ_WQE_BYTE_4_FENCE_S,
@@ -1082,6 +1078,11 @@ static int set_rc_wqe(void *wqe, struct hns_roce_qp *qp, struct ibv_send_wr *wr,
 		     !!(wr->send_flags & IBV_SEND_SOLICITED));
 	roce_set_bit(rc_sq_wqe->byte_4, RC_SQ_WQE_BYTE_4_INLINE_S,
 		     !!(wr->send_flags & IBV_SEND_INLINE));
+	roce_set_bit(rc_sq_wqe->byte_4, RC_SQ_WQE_BYTE_4_SO_S, 0);
+
+	ret = check_rc_opcode(rc_sq_wqe, wr);
+	if (ret)
+		return ret;
 
 	roce_set_field(rc_sq_wqe->byte_20,
 		       RC_SQ_WQE_BYTE_20_MSG_START_SGE_IDX_M,
