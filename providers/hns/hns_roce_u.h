2571eb0e83836ea7aec0d184673daf2d126aa72c
Wenpeng Liang <liangwenpeng@huawei.com>
2021-12-31 18:01:06 +0800

    libhns: Fix the calculation of QP/SRQ table size
    [ Upstream commit f3596c3f842a3269cb238d7ba607302b81baf9c1 ]
    
    The table_size means the maximum number of QP/SRQ. This value may not be
    a power of two. The old algorithm will lead to a result that allocates a
    mismatched table.
    
    Fixes: 887b78c80224 ("libhns: Add initial main frame")
    Fixes: 9e3df7578153 ("libhns: Support ibv_create_srq_ex")
    Signed-off-by: Wenpeng Liang <liangwenpeng@huawei.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- providers/hns/hns_roce_u.h
+++ providers/hns/hns_roce_u.h
@@ -36,6 +36,7 @@
 #include <stddef.h>
 #include <endian.h>
 #include <util/compiler.h>
+#include <util/util.h>
 
 #include <infiniband/driver.h>
 #include <util/udma_barrier.h>
@@ -71,10 +72,8 @@
 #define roce_set_bit(origin, shift, val) \
 	roce_set_field((origin), (1ul << (shift)), (shift), (val))
 
-enum {
-	HNS_ROCE_QP_TABLE_BITS		= 8,
-	HNS_ROCE_QP_TABLE_SIZE		= 1 << HNS_ROCE_QP_TABLE_BITS,
-};
+#define HNS_ROCE_QP_TABLE_BITS 8
+#define HNS_ROCE_QP_TABLE_SIZE BIT(HNS_ROCE_QP_TABLE_BITS)
 
 /* operation type list */
 enum {
@@ -124,7 +123,6 @@ struct hns_roce_context {
 
 	pthread_mutex_t			qp_table_mutex;
 
-	int				num_qps;
 	int				qp_table_shift;
 	int				qp_table_mask;
 
@@ -226,14 +224,15 @@ struct hns_roce_u_hw {
 	struct verbs_context_ops hw_ops;
 };
 
-static inline unsigned long align(unsigned long val, unsigned long align)
-{
-	return (val + align - 1) & ~(align - 1);
-}
-
 static inline unsigned int hr_ilog32(unsigned int count)
 {
 	return ilog32(count - 1);
+}
+
+static inline uint32_t to_hr_qp_table_index(uint32_t qpn,
+					    struct hns_roce_context *ctx)
+{
+	return (qpn >> ctx->qp_table_shift) & (HNS_ROCE_QP_TABLE_SIZE - 1);
 }
 
 static inline struct hns_roce_device *to_hr_dev(struct ibv_device *ibv_dev)
