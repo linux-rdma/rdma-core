0b122a1e86595ea56d1ef7deb925825bed8213e1
Chengchang Tang <tangchengchang@huawei.com>
2022-04-08 11:31:07 +0800

    libhns: Fix the shift size of SQ WQE
    [ Upstream commit 847336b7634b51548996b879f42c786a108885f1 ]
    
    Currently, the shift size of SQ WQE is based on the size of the SQ WQE
    structure of HIP06. Although the size of SQ WQE of HIP08 is the same as
    the size of SQ WQE of HIP06, it is not a correct way for HIP08 to use the
    structure of HIP06 to define the size of SQ WQE.
    
    Fixes: b6cd213b276f ("libhns: Refactor for creating qp")
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Wenpeng Liang <liangwenpeng@huawei.com>
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- providers/hns/hns_roce_u_verbs.c
+++ providers/hns/hns_roce_u_verbs.c
@@ -997,8 +997,7 @@ static void hns_roce_set_qp_params(struct ibv_qp_init_attr_ex *attr,
 	}
 
 	if (attr->cap.max_send_wr) {
-		qp->sq.wqe_shift =
-			hr_ilog32(sizeof(struct hns_roce_rc_send_wqe));
+		qp->sq.wqe_shift = HNS_ROCE_SQWQE_SHIFT;
 		cnt = roundup_pow_of_two(attr->cap.max_send_wr);
 		qp->sq.wqe_cnt = cnt;
 		qp->sq.shift = hr_ilog32(cnt);
