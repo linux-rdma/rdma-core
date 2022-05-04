fa2fafb6db6373f54463eb83754ba6c1a8900ef0
Håkon Bugge <haakon.bugge@oracle.com>
2022-05-04 14:17:44 +0200

    rdma_xserver/xclient: Make recv/send WQs disjunctive
    [ Upstream commit 8393ea111e68386890bd0a878d3976815f3eac24 ]
    
    Running rdma_xclient on a system with CX-5, yields the following
    error:
    
    $ rdma_xclient -s 192.168.77.1 -c x
    rdma_xclient: start
    rdma_create_ep: Invalid argument
    rdma_xclient: end -1
    
    Enabling dynamic tracing of the mlx5/qp.c file gives:
    
    infiniband mlx5_0: create_qp:3035:(pid 53349): Create QP type 9 failed
    
    This is because the rdma_xclient sets the max_recv_{wr,sge} in the
    ibv_qp_init_attr struct before calling rdma_create_ep().
    
    An XRC_SEND QP type shall not have a receive queue and an XRC_RECV QP
    type shall not have a send queue.
    
    Fixed by only configuring the send/recv queue in rdma_xserver/xclient
    respectively.
    
    Fixes: 6bda3d049bcf ("rdma_xserver/client: Add new test apps")
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Cc: stable@linux-rdma.org # v34.x
    
    ---
    v1 -> v2:
       * Made sure QP type RC continue to work
       * Added Cc: tag
    
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- librdmacm/examples/rdma_xclient.c
+++ librdmacm/examples/rdma_xclient.c
@@ -87,8 +87,12 @@ static int test(void)
 	}
 
 	memset(&attr, 0, sizeof attr);
-	attr.cap.max_send_wr = attr.cap.max_recv_wr = 1;
-	attr.cap.max_send_sge = attr.cap.max_recv_sge = 1;
+	attr.cap.max_send_wr = 1;
+	attr.cap.max_send_sge = 1;
+	if (hints.ai_qp_type != IBV_QPT_XRC_SEND) {
+		attr.cap.max_recv_wr = 1;
+		attr.cap.max_recv_sge = 1;
+	}
 	attr.sq_sig_all = 1;
 	ret = rdma_create_ep(&id, res, NULL, &attr);
 	rdma_freeaddrinfo(res);
