172c3ff9d47b3770043bf2ad8fedc4804374e1c7
Jason Gunthorpe <jgg@nvidia.com>
2022-11-25 13:36:43 -0400

    sparse: Fixup gen-sparse.py to generate the diffs properly
    This didn't work on newer glibcs with a subset of the patch files, and it
    needs to strip path prefixes when building the diff.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- buildlib/RDMA_Sparse.cmake
+++ buildlib/RDMA_Sparse.cmake
@@ -26,7 +26,7 @@ int main(int argc,const char *argv[]) {return 0;}
       RESULT_VARIABLE retcode)
     if(NOT "${retcode}" STREQUAL "0")
       message(FATAL_ERROR "glibc header file patching for sparse failed. Review include/*.rej and fix the rejects, then do "
-	"${BUILDLIB}/gen-sparse.py -out ${BUILD_INCLUDE}/ --src ${PROJECT_SOURCE_DIR}/ --save")
+	"${BUILDLIB}/gen-sparse.py --out ${BUILD_INCLUDE}/ --src ${PROJECT_SOURCE_DIR}/ --save")
     endif()
 
     # Enable endian analysis in sparse
