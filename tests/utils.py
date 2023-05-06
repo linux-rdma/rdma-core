ba9f41ab5f84e758faf226778ed0cdfce106ba8a
Yonatan Nachum <ynachum@amazon.com>
2023-05-06 16:42:26 +0000

    tests: Add RDMA write with imm opcode to send util
    RDMA write with imm opcode is missing from the condition to set rkey and
    address of remote MR in creation of WR util function. Add it to the
    condition.
    
    Signed-off-by: Yonatan Nachum <ynachum@amazon.com>

--- tests/utils.py
+++ tests/utils.py
@@ -416,7 +416,7 @@ def get_send_elements(agr_obj, is_server, opcode=e.IBV_WR_SEND):
     agr_obj.mem_write(msg, agr_obj.msg_size + offset)
     sge = SGE(agr_obj.mr.buf + offset, agr_obj.msg_size, agr_obj.mr_lkey)
     send_wr = SendWR(opcode=opcode, num_sge=1, sg=[sge])
-    if opcode in [e.IBV_WR_RDMA_WRITE, e.IBV_WR_RDMA_READ]:
+    if opcode in [e.IBV_WR_RDMA_WRITE, e.IBV_WR_RDMA_WRITE_WITH_IMM, e.IBV_WR_RDMA_READ]:
         send_wr.set_wr_rdma(int(agr_obj.rkey), int(agr_obj.remote_addr))
     return send_wr, sge
 
