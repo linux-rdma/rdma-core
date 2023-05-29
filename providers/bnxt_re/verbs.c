665e779582476c3cd3443b22c6f486c42ed5e6ad
Selvin Xavier <selvin.xavier@broadcom.com>
2023-05-29 03:02:54 -0700

    bnxt_re/lib: Do not add QP to error list from terminal CQE if user destroys the QP
    During destroy_qp, if the HW  has outstanding requests,
    it generates terminal CQEs.Since the library is adding
    the QP to flush list, we are completing these outstanding
    request in flush error. Some applications considers this as
    error and exits the application.
    
    Adding a check to avoid reporting flush completions if the
    application is destroying the QP. Reset the state before
    sending destroy_qp and check for the state before adding
    the QP to flush list from terminal cqe processing.
    
    Signed-off-by: Chandramohan Akula <chandramohan.akula@broadcom.com>
    Signed-off-by: Selvin Xavier <selvin.xavier@broadcom.com>

--- providers/bnxt_re/verbs.c
+++ providers/bnxt_re/verbs.c
@@ -615,13 +615,16 @@ static uint8_t bnxt_re_poll_term_cqe(struct bnxt_re_qp *qp,
 	 * SQ and RQ are empty.
 	 */
 	*cnt = 0;
+	/* If the QP is destroyed, avoid handling this QP as flushlist */
+	if (qp->qpst == IBV_QPS_RESET)
+		goto exit;
 	if (qp->qpst != IBV_QPS_ERR)
 		qp->qpst = IBV_QPS_ERR;
 	pthread_spin_lock(&cntx->fqlock);
 	bnxt_re_fque_add_node(&rcq->rfhead, &qp->rnode);
 	bnxt_re_fque_add_node(&scq->sfhead, &qp->snode);
 	pthread_spin_unlock(&cntx->fqlock);
-
+exit:
 	return pcqe;
 }
 
@@ -1301,6 +1304,7 @@ int bnxt_re_destroy_qp(struct ibv_qp *ibvqp)
 	struct bnxt_re_qp *qp = to_bnxt_re_qp(ibvqp);
 	int status;
 
+	qp->qpst = IBV_QPS_RESET;
 	status = ibv_cmd_destroy_qp(ibvqp);
 	if (status)
 		return status;
