7422a3d182e7eaa2ac4547fe30c4c0dc67300d09
Mustafa Ismail <mustafa.ismail@intel.com>
2024-05-13 15:29:48 -0500

    providers/irdma: Do not override the raw op type in CQE
    [ Upstream commit 7f2d095ff4739c03b4e7c6f0a399100feb3725f2 ]
    
    Do not override the raw op type in RQ CQ completions,
    because the opcode is written by HW in the CQE and
    it matches the protocol opcode from the received
    packet.
    
    Fixes: c8b3aca ("providers/irdma: Fix RQ completion opcode")
    Signed-off-by: Mustafa Ismail <mustafa.ismail@intel.com>
    Signed-off-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>
    Signed-off-by: Nicolas Morey <nmorey@suse.com>

--- providers/irdma/uk.c
+++ providers/irdma/uk.c
@@ -1218,10 +1218,6 @@ irdma_uk_cq_poll_cmpl(struct irdma_cq_uk *cq, struct irdma_cq_poll_info *info)
 
 		info->bytes_xfered = (__u32)FIELD_GET(IRDMACQ_PAYLDLEN, qword0);
 
-		if (info->imm_valid)
-			info->op_type = IRDMA_OP_TYPE_REC_IMM;
-		else
-			info->op_type = IRDMA_OP_TYPE_REC;
 		if (qword3 & IRDMACQ_STAG) {
 			info->stag_invalid_set = true;
 			info->inv_stag = (__u32)FIELD_GET(IRDMACQ_INVSTAG, qword2);
