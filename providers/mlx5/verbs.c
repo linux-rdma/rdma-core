70358099bcaed5e1d789c078d664a79f1536668f
Edward Srouji <edwards@nvidia.com>
2024-07-15 15:11:27 +0300

    mlx5: Remove max_post assignment in create_qp()
    In create_qp(), RQ max_post value was redundantly overwritten after
    being calculated already as part of mlx5_calc_rq_size().
    
    This assumption that max_post is equal to wqe_cnt is not necessarily
    true in all cases, as will be changed in upcoming patches where the OOO
    RX support feature is introduced.
    
    The line was removed, and the calculation is now handled by
    mlx5_calc_rq_size() without overwriting its calculation.
    
    Signed-off-by: Edward Srouji <edwards@nvidia.com>
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>

--- providers/mlx5/verbs.c
+++ providers/mlx5/verbs.c
@@ -2638,7 +2638,6 @@ static struct ibv_qp *create_qp(struct ibv_context *context,
 	qp->get_ece = resp_drv->ece_options;
 	map_uuar(context, qp, resp_drv->bfreg_index, bf);
 
-	qp->rq.max_post = qp->rq.wqe_cnt;
 	if (attr->sq_sig_all)
 		qp->sq_signal_bits = MLX5_WQE_CTRL_CQ_UPDATE;
 	else
