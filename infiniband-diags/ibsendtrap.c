e949978b22707a217d1a907307810b4e20965a07
Asaf Mazor <amazor@nvidia.com>
2025-01-14 17:30:40 +0200

    ibsendtrap: Use API supporting SMI/GSI seperation
    Migration to the SMI/GSI separated API to support planarized networks.
    
    Signed-off-by: Asaf Mazor <amazor@nvidia.com>

--- infiniband-diags/ibsendtrap.c
+++ infiniband-diags/ibsendtrap.c
@@ -49,6 +49,7 @@
 #include "ibdiag_common.h"
 
 static struct ibmad_port *srcport;
+static struct ibmad_ports_pair *srcports;
 /* for local link integrity */
 static int error_port = 1;
 
@@ -185,10 +186,10 @@ static int send_trap(void (*build) (ib_mad_notice_attr_t *, ib_portid_t *))
 	ib_rpc_t trap_rpc;
 	ib_mad_notice_attr_t notice;
 
-	if (resolve_self(ibd_ca, ibd_ca_port, &selfportid, &selfport, NULL))
+	if (resolve_self(srcports->smi.ca_name, ibd_ca_port, &selfportid, &selfport, NULL))
 		IBEXIT("can't resolve self");
 
-	if (resolve_sm_portid(ibd_ca, ibd_ca_port, &sm_port))
+	if (resolve_sm_portid(srcports->smi.ca_name, ibd_ca_port, &sm_port))
 		IBEXIT("can't resolve SM destination port");
 
 	memset(&trap_rpc, 0, sizeof(trap_rpc));
@@ -264,13 +265,16 @@ int main(int argc, char **argv)
 
 	madrpc_show_errors(1);
 
-	srcport = mad_rpc_open_port(ibd_ca, ibd_ca_port, mgmt_classes, 2);
+	srcports = mad_rpc_open_port2(ibd_ca, ibd_ca_port, mgmt_classes, 2, 1);
+	if (!srcports)
+		IBEXIT("Failed to open '%s' port '%d'", ibd_ca, ibd_ca_port);
+	srcport = srcports->smi.port;
 	if (!srcport)
 		IBEXIT("Failed to open '%s' port '%d'", ibd_ca, ibd_ca_port);
 
 	smp_mkey_set(srcport, ibd_mkey);
 
 	rc = process_send_trap(trap_name);
-	mad_rpc_close_port(srcport);
+	mad_rpc_close_port2(srcports);
 	return rc;
 }
