59801402dfeeac9385fc3582f253f19385bf797b
Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
2025-06-17 15:40:49 +0530

    bnxt_re/lib: Direct Verbs: Support CQ verbs
    The following Direct Verb (DV) API has been implemented in
    this patch, by enhancing the driver specific udata in the
    existing verb.
    
    - bnxt_re_dv_create_cq():
      Create a CQ of requested size (cqe). The application must have
      already associated this memory with an ibv_buf using
      ibv_alloc_user_buf(). The CQ's umem_handle and umem_offset
      provided by the application are translated into an address for
      mapping and passed to the driver.
    
    Note:
    Some applications might want to allocate memory for all CQs in one big
    chunk and associate that entire memory with a single ibv_buf using
    ibv_alloc_user_buf(). At the time of creating each individual resource,
    the application should pass a specific offset/length within that
    memory.
    
    Signed-off-by: Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
    Co-developed-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Selvin Xavier <selvin.xavier@broadcom.com>

--- providers/bnxt_re/dv.c
+++ providers/bnxt_re/dv.c
@@ -139,3 +139,98 @@ bnxt_re_dv_alloc_db_region(struct ibv_context *ctx)
 
 	return out;
 }
+
+static bool bnxt_re_dv_is_valid_buf(struct bnxt_re_dev *dev,
+				    struct ibv_buf *buf,
+				    uint64_t offset, uint32_t size)
+{
+	return ((offset == align(offset, dev->pg_size)) &&
+		(offset + size <= buf->size));
+}
+
+static int bnxt_re_dv_create_cq_cmd(struct bnxt_re_dev *dev,
+				    struct ibv_context *ibvctx,
+				    struct bnxt_re_cq *cq,
+				    struct bnxt_re_dv_cq_init_attr *cq_attr,
+				    uint64_t comp_mask,
+				    struct ubnxt_re_cq_resp *resp)
+{
+	struct bnxt_re_context *cntx = to_bnxt_re_context(ibvctx);
+	DECLARE_COMMAND_BUFFER(driver_attrs, UVERBS_OBJECT_CQ,
+			       UVERBS_METHOD_CQ_CREATE, 1);
+	struct ib_uverbs_buffer_desc cq_buf_umem_desc;
+	struct ibv_cq_init_attr_ex cq_attr_ex = {};
+	uint64_t offset = cq_attr->umem_offset;
+	struct ibv_buf *cq_buf = cq->cq_buf;
+	struct ubnxt_re_cq cmd = {};
+	void *buf_addr;
+	uint32_t size;
+	int ret;
+
+	size = cq_attr->ncqe * bnxt_re_get_cqe_sz();
+	if (!bnxt_re_dv_is_valid_buf(dev, cq_buf, offset, size))
+		return -EINVAL;
+
+	cmd.cq_handle = (uintptr_t)cq;
+	cmd.comp_mask = comp_mask;
+
+	buf_addr = (uint8_t *)cq_buf->addr + offset;
+	cmd.cq_va = (uintptr_t)buf_addr;
+	fill_attr_in_buf_umem(driver_attrs, UVERBS_ATTR_CREATE_CQ_BUF_UMEM,
+			      &cq_buf_umem_desc, cq_buf,
+			      buf_addr, size);
+
+	cq_attr_ex.cqe = cq_attr->ncqe;
+	cq_attr_ex.comp_mask = 0;
+	cq_attr_ex.flags = 0;
+
+	memset(resp, 0, sizeof(*resp));
+	ret = ibv_cmd_create_cq_ex2(ibvctx, &cq_attr_ex, NULL,
+				    &cq->verbs_cq,
+				    &cmd.ibv_cmd, sizeof(cmd),
+				    &resp->ibv_resp,
+				    sizeof(*resp), 0, driver_attrs);
+	if (ret)
+		return ret;
+
+	cq->cqid = resp->cqid;
+	cq->phase = resp->phase;
+	cq->udpi = &cntx->udpi;
+	cq->cntx = cntx;
+	cq->rand.seed = cq->cqid;
+
+	return 0;
+}
+
+struct ibv_cq *bnxt_re_dv_create_cq(struct ibv_context *ibvctx,
+				    struct bnxt_re_dv_cq_init_attr *cq_attr)
+{
+	struct bnxt_re_dev *dev = to_bnxt_re_dev(ibvctx->device);
+	uint64_t comp_mask = BNXT_RE_CQ_FIXED_NUM_CQE_ENABLE;
+	struct ibv_buf *cq_buf = cq_attr->umem_handle;
+	struct ubnxt_re_cq_resp resp = {};
+	struct bnxt_re_cq *cq;
+	int ret;
+
+	if (!(dev->vdev.core_support & IB_UVERBS_CORE_SUPPORT_ROBUST_UDATA))
+		return NULL;
+
+	if (cq_attr->ncqe > dev->max_cq_depth)
+		return NULL;
+
+	cq = calloc(1, (sizeof(*cq)));
+	if (!cq)
+		return NULL;
+
+	cq->cq_buf = cq_buf;
+	ret = bnxt_re_dv_create_cq_cmd(dev, ibvctx, cq, cq_attr, comp_mask, &resp);
+	if (ret)
+		goto fail;
+
+	cq->dv_cq_flags |= BNXT_DV_CQ_FLAGS_VALID;
+	return &cq->verbs_cq.cq;
+
+fail:
+	free(cq);
+	return NULL;
+}
