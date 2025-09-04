linux-rdma/rdma-core

commit aaf9f699c10f266c7b3b8bb6af7af19302f73873

ibqueryerrors: Use API supporting SMI/GSI seperation
Migration to the SMI/GSI separated API to support planarized networks.

Change sa_get_handle function to accept ca_name as a parameter

Signed-off-by: Asaf Mazor <amazor@nvidia.com>

infiniband-diags/ibdiag_sa.c
@@ -48,17 +48,19 @@
  * the saquery tool and provides it to other utilities.
  */
 
-struct sa_handle * sa_get_handle(void)
+struct sa_handle *sa_get_handle(char *ca_name)
 {
-	struct sa_handle * handle;
+	struct sa_handle *handle;
 	handle = calloc(1, sizeof(*handle));
 	if (!handle)
 		IBPANIC("calloc failed");
 
-	resolve_sm_portid(ibd_ca, ibd_ca_port, &handle->dport);
+	char *name = ca_name ? ca_name : ibd_ca;
+
+	resolve_sm_portid(name, ibd_ca_port, &handle->dport);
 	if (!handle->dport.lid) {
 		IBWARN("No SM/SA found on port %s:%d",
-			ibd_ca ? "" : ibd_ca,
+			name ? "" : name,
 			ibd_ca_port);
 		goto err;
 	}
@@ -67,16 +69,17 @@ struct sa_handle * sa_get_handle(void)
 	if (!handle->dport.qkey)
 		handle->dport.qkey = IB_DEFAULT_QP1_QKEY;
 
-	if ((handle->fd = umad_open_port(ibd_ca, ibd_ca_port)) < 0) {
+	handle->fd = umad_open_port(name, ibd_ca_port);
+	if (handle->fd < 0) {
 		IBWARN("umad_open_port on port %s:%d failed",
-			ibd_ca ? "" : ibd_ca,
+			name ? "" : name,
 			ibd_ca_port);
 		goto err;
 	}
 	if ((handle->agent = umad_register(handle->fd, IB_SA_CLASS, 2, 1, NULL)) < 0) {
 		umad_close_port(handle->fd);
 		IBWARN("umad_register for SA class failed on port %s:%d",
-		       ibd_ca ? "" : ibd_ca,
+		       name ? "" : name,
 		       ibd_ca_port);
 		goto err;
 	}

infiniband-diags/ibqueryerrors.c
@@ -55,6 +55,7 @@
 #include "ibdiag_sa.h"
 
 static struct ibmad_port *ibmad_port;
+static struct ibmad_ports_pair *ibmad_ports;
 static char *node_name_map_file = NULL;
 static nn_map_t *node_name_map = NULL;
 static char *load_cache_file = NULL;
@@ -319,9 +320,10 @@ static int path_record_query(ib_gid_t sgid,uint64_t dguid)
      ib_path_rec_t pr;
      __be64 comp_mask = 0;
      uint8_t reversible = 0;
-     struct sa_handle * h;
+     struct sa_handle *h;
 
-     if (!(h = sa_get_handle()))
+	h = sa_get_handle(ibmad_ports->gsi.ca_name);
+	if (!h)
 	return -1;
 
      ibd_timeout = DEFAULT_HALF_WORLD_PR_TIMEOUT;
@@ -1026,9 +1028,11 @@ int main(int argc, char **argv)
 	if (!node_type_to_print)
 		node_type_to_print = PRINT_ALL;
 
-	ibmad_port = mad_rpc_open_port(ibd_ca, ibd_ca_port, mgmt_classes, 4);
-	if (!ibmad_port)
+	ibmad_ports = mad_rpc_open_port2(ibd_ca, ibd_ca_port, mgmt_classes, 4, 0);
+	if (!ibmad_ports)
 		IBEXIT("Failed to open port; %s:%d\n", ibd_ca, ibd_ca_port);
+
+	ibmad_port = ibmad_ports->gsi.port;
 
 	smp_mkey_set(ibmad_port, ibd_mkey);
 
@@ -1041,13 +1045,14 @@ int main(int argc, char **argv)
 	config.mkey = ibd_mkey;
 
 	if (dr_path && load_cache_file) {
-		mad_rpc_close_port(ibmad_port);
+		mad_rpc_close_port2(ibmad_ports);
 		fprintf(stderr, "Cannot specify cache and direct route path\n");
 		exit(-1);
 	}
 
-	if (resolve_self(ibd_ca, ibd_ca_port, &self_portid, &port, &self_gid.raw) < 0) {
-		mad_rpc_close_port(ibmad_port);
+	if (resolve_self(ibmad_ports->gsi.ca_name, ibd_ca_port,
+			&self_portid, &port, &self_gid.raw) < 0) {
+		mad_rpc_close_port2(ibmad_ports);
 		IBEXIT("can't resolve self port %s", argv[0]);
 	}
 
@@ -1056,13 +1061,13 @@ int main(int argc, char **argv)
 	/* limit the scan the fabric around the target */
 	if (dr_path) {
 		if ((resolved =
-		     resolve_portid_str(ibd_ca, ibd_ca_port, &portid, dr_path,
+		     resolve_portid_str(ibmad_ports->gsi.ca_name, ibd_ca_port, &portid, dr_path,
 					IB_DEST_DRPATH, NULL, ibmad_port)) < 0)
 			IBWARN("Failed to resolve %s; attempting full scan",
 			       dr_path);
 	} else if (port_guid_str) {
 		if ((resolved =
-		     resolve_portid_str(ibd_ca, ibd_ca_port, &portid,
+		     resolve_portid_str(ibmad_ports->gsi.ca_name, ibd_ca_port, &portid,
 					port_guid_str, IB_DEST_GUID, ibd_sm_id,
 					       ibmad_port)) < 0)
 			IBWARN("Failed to resolve %s; attempting full scan",
@@ -1071,7 +1076,7 @@ int main(int argc, char **argv)
 			lid2sl_table[portid.lid] = portid.sl;
 	}
 
-	mad_rpc_close_port(ibmad_port);
+	mad_rpc_close_port2(ibmad_ports);
 
 	if (load_cache_file) {
 		if ((fabric = ibnd_load_fabric(load_cache_file, 0)) == NULL) {
@@ -1102,8 +1107,16 @@ int main(int argc, char **argv)
 	set_thresholds();
 
 	/* reopen the global ibmad_port */
-	ibmad_port = mad_rpc_open_port(ibd_ca, ibd_ca_port,
-				       mgmt_classes, 4);
+	ibmad_ports = mad_rpc_open_port2(ibd_ca, ibd_ca_port,
+				       mgmt_classes, 4, dr_path ? 1 : 0);
+	if (!ibmad_ports) {
+		ibnd_destroy_fabric(fabric);
+		close_node_name_map(node_name_map);
+		IBEXIT("Failed to reopen port: %s:%d\n",
+			ibd_ca, ibd_ca_port);
+	}
+
+	ibmad_port = ibmad_ports->gsi.port;
 	if (!ibmad_port) {
 		ibnd_destroy_fabric(fabric);
 		close_node_name_map(node_name_map);
@@ -1156,7 +1169,7 @@ int main(int argc, char **argv)
 		rc = 1;
 
 close_port:
-	mad_rpc_close_port(ibmad_port);
+	mad_rpc_close_port2(ibmad_ports);
 	ibnd_destroy_fabric(fabric);
 
 close_name_map:

infiniband-diags/ibdiag_sa.h
@@ -59,7 +59,7 @@ struct sa_query_result {
 };
 
 /* NOTE: umad_init must be called prior to sa_get_handle */
-struct sa_handle * sa_get_handle(void);
+struct sa_handle *sa_get_handle(char *ca_name);
 void sa_free_handle(struct sa_handle * h);
 
 int sa_query(struct sa_handle *h, uint8_t method,

infiniband-diags/saquery.c
@@ -1859,7 +1859,7 @@ int main(int argc, char **argv)
 	if (umad_init())
 		IBEXIT("Failed to initialized umad library");
 
-	h = sa_get_handle();
+	h = sa_get_handle(NULL);
 	if (!h)
 		IBPANIC("Failed to bind to the SA");
 

commit 7e09ca1b997f44ea0024107025a555a93fedb5c9

ibqueryerrors: Fix SMP call to use correct port
- Change to correct port on smp_query_via call

Fixes: aaf9f699c10f ("ibqueryerrors: Use API supporting SMI/GSI seperation")
Signed-off-by: Asaf Mazor <amazor@nvidia.com>

infiniband-diags/ibdiag_sa.c
@@ -38,8 +38,8 @@
 
 
 #include <errno.h>
+#include <infiniband/mad.h>
 #include <infiniband/umad.h>
-#include "../libibmad/mad_internal.h"
 
 #include "ibdiag_common.h"
 #include "ibdiag_sa.h"
@@ -78,8 +78,8 @@ struct sa_handle *sa_get_handle(char *ca_name)
 	if (!handle->dport.qkey)
 		handle->dport.qkey = IB_DEFAULT_QP1_QKEY;
 
-	handle->fd = srcports->gsi.port->port_id;
-	handle->agent = srcports->gsi.port->class_agents[IB_SA_CLASS];
+	handle->fd = mad_rpc_portid(srcports->gsi.port);
+	handle->agent = mad_rpc_class_agent(srcports->gsi.port, IB_SA_CLASS);
 
 	return handle;
 

infiniband-diags/ibqueryerrors.c
@@ -1124,10 +1124,12 @@ int main(int argc, char **argv)
 			ibd_ca, ibd_ca_port);
 	}
 
-	smp_mkey_set(ibmad_port, ibd_mkey);
+	smp_mkey_set(ibmad_ports->smi.port, ibd_mkey);
 
-	if (ibd_timeout)
-		mad_rpc_set_timeout(ibmad_port, ibd_timeout);
+	if (ibd_timeout) {
+		mad_rpc_set_timeout(ibmad_ports->smi.port, ibd_timeout);
+		mad_rpc_set_timeout(ibmad_ports->gsi.port, ibd_timeout);
+	}
 
 	if (port_guid_str) {
 		ibnd_port_t *ndport = ibnd_find_port_guid(fabric, port_guid);
@@ -1141,7 +1143,7 @@ int main(int argc, char **argv)
 
 		uint8_t ni[IB_SMP_DATA_SIZE] = { 0 };
 		if (!smp_query_via(ni, &portid, IB_ATTR_NODE_INFO, 0,
-			   ibd_timeout, ibmad_port)) {
+			   ibd_timeout, ibmad_ports->smi.port)) {
 				fprintf(stderr, "Failed to query local Node Info\n");
 				goto close_port;
 		}